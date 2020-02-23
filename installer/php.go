7eab764c37614c424e3228f677bc0b7091a7dd9a
Jason McCallister <jason@craftcms.com>
2020-02-22 19:23:07 -0500

    move installer to sep package
    Signed-off-by: Jason McCallister <jason@craftcms.com>

---
+++ installer/php.go
@@ -0,0 +1,46 @@
+package installer
+
+import "errors"
+
+// PHP takes a version and returns the commands that
+// are used to install that version of PHP. If an
+// unknown version is passed, it will return an error
+func PHP(v string) ([]string, error) {
+	switch v {
+	case "7.4":
+		return []string{
+			"php7.4",
+			"php7.4-mbstring",
+			"php7.4-cli",
+			"php7.4-curl",
+			"php7.4-fpm",
+			"php7.4-gd",
+			"php7.4-intl",
+			"php7.4-json",
+			"php7.4-mysql",
+			"php7.4-opcache",
+			"php7.4-pgsql",
+			"php7.4-zip",
+			"php7.4-xml",
+		}, nil
+	case "7.3":
+		return []string{
+			"php7.3",
+			"php7.3-mbstring",
+			"php7.3-cli",
+			"php7.3-curl",
+			"php7.3-fpm",
+			"php7.3-gd",
+			"php7.3-intl",
+			"php7.3-json",
+			"php7.3-mysql",
+			"php7.3-opcache",
+			"php7.3-pgsql",
+			"php7.3-zip",
+			"php7.3-xml",
+		}, nil
+	default:
+		return []string{}, errors.New("unsupported version of PHP provided")
+	}
+	return []string{}, nil
+}
---
+++ installer/php_test.go
@@ -0,0 +1,39 @@
+package installer
+
+import "testing"
+
+func TestPHPReturnsCommands(t *testing.T) {
+	// Arrange
+	v := "7.4"
+	expected := 13
+
+	// Act
+	commands, err := PHP(v)
+	total := len(commands)
+
+	// Assert
+	if err != nil {
+		t.Errorf("expected the error to be nil, got %v", err.Error())
+	}
+	if total != expected {
+		t.Errorf("expected total number of commands to be %v, got %v instead", expected, total)
+	}
+}
+
+func TestPHPReturnsError(t *testing.T) {
+	// Arrange
+	v := "nothere"
+	expected := 0
+
+	// Act
+	commands, err := PHP(v)
+	total := len(commands)
+
+	// Assert
+	if err == nil {
+		t.Errorf("expected the error to NOT be nil, got %v", err.Error())
+	}
+	if total != expected {
+		t.Errorf("expected total number of commands to be %v, got %v instead", expected, total)
+	}
+}
