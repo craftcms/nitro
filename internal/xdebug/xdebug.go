93ae50d8363921d29fca331601fa26f2f79a7acb
Jason McCallister <jason@craftcms.com>
2020-03-03 13:49:50 -0500

    Switch to xon and xoff
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/xdebug/xdebug.go
+++ internal/xdebug/xdebug.go
@@ -6,21 +6,24 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func Command() *cli.Command {
+func CommandOn() *cli.Command {
 	return &cli.Command{
-		Name:        "xdebug",
+		Name:        "xon",
 		Usage:       "Enable Xdebug",
-		Description: "Calling Xdebug will default to enabling the extension, if the flag --disable is passed it will be disabled",
+		Description: "Enable Xdebug for machine",
 		Action: func(c *cli.Context) error {
 			return errors.New("not implemented")
 		},
-		Flags: []cli.Flag{
-			&cli.BoolFlag{
-				Name:        "disable",
-				Usage:       "Disable Xdebug",
-				Value:       false,
-				DefaultText: "false",
-			},
+	}
+}
+
+func CommandOff() *cli.Command {
+	return &cli.Command{
+		Name:        "xoff",
+		Usage:       "Disable Xdebug",
+		Description: "Disable Xdebug on machine",
+		Action: func(c *cli.Context) error {
+			return errors.New("not implemented")
 		},
 	}
 }
