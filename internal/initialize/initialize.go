a356d1c50343bb0b8d32a28004b70460047d0d4f
Jason McCallister <jason@craftcms.com>
2020-03-04 22:05:23 -0500

    Add php-imagick to each php version installer
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/initialize/initialize.go
+++ internal/initialize/initialize.go
@@ -78,17 +78,17 @@ write_files:
     content: |
       #!/bin/bash
       apt install -y php7.4 php7.4-mbstring php7.4-cli php7.4-curl php7.4-fpm php7.4-gd php7.4-intl php7.4-json \
-      php7.4-mysql php7.4-opcache php7.4-pgsql php7.4-zip php7.4-xml php-xdebug
+      php7.4-mysql php7.4-opcache php7.4-pgsql php7.4-zip php7.4-xml php-xdebug php-imagick
   - path: /opt/nitro/php/php-73.sh
     content: |
       #!/bin/bash
       apt install -y php7.3 php7.3-mbstring php7.3-cli php7.3-curl php7.3-fpm php7.3-gd php7.3-intl php7.3-json \
-      php7.3-mysql php7.3-opcache php7.3-pgsql php7.3-zip php7.3-xml php-xdebug
+      php7.3-mysql php7.3-opcache php7.3-pgsql php7.3-zip php7.3-xml php-xdebug php-imagick
   - path: /opt/nitro/php/php-72.sh
     content: |
       #!/bin/bash
       apt install -y php7.2 php7.2-mbstring php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-json \
-      php7.2-mysql php7.2-opcache php7.2-pgsql php7.2-zip php7.2-xml php-xdebug
+      php7.2-mysql php7.2-opcache php7.2-pgsql php7.2-zip php7.2-xml php-xdebug php-imagick
   # create the composer install script
   - path: /opt/nitro/composer-install.sh
     content: |
