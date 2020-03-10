0797a5a7913236f62a786012370e23499ce6a5e1
Jason McCallister <jason@craftcms.com>
2020-03-10 11:36:06 -0400

    fix bootstrap from refactor
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/command/bootstrap.go
+++ internal/command/bootstrap.go
@@ -1,38 +1,47 @@
 package command
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 
 	"github.com/urfave/cli/v2"
+
+	"github.com/craftcms/nitro/internal/validate"
 )
 
 // Bootstrap will install the requirements
 func Bootstrap() *cli.Command {
 	return &cli.Command{
-		Name:        "delete",
+		Name:        "bootstrap",
 		Usage:       "Delete machine",
 		Description: "Delete a machine when no longer needed, this is recoverable and not permanently deleted.",
 		Before:      bootstrapBeforeAction,
 		Action:      bootstrapAction,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "php-version",
+				Usage:       "Provide version of PHP",
+				Value:       "7.4",
+				DefaultText: "7.4",
+			},
+			&cli.StringFlag{
+				Name:        "database",
+				Usage:       "Provide version of PHP",
+				Value:       "mariadb",
+				DefaultText: "mariadb",
+			},
+		},
 	}
 }
 
 func bootstrapBeforeAction(c *cli.Context) error {
-	rdr := bufio.NewReader(os.Stdin)
-	fmt.Print("WARNING: Are you sure you wish to perform this task (y/N)? ")
-
-	answer, err := rdr.ReadString(' ')
-	if err != nil {
+	if err := validate.PHPVersion(c.String("php-version")); err != nil {
 		return err
 	}
 
-	if strings.Contains(answer, "n") {
-		fmt.Println("Skipping the deletion of", c.String("machine"))
-		return nil
+	if err := validate.Database(c.String("database")); err != nil {
+		return err
 	}
 
 	return nil
@@ -40,9 +49,23 @@ func bootstrapBeforeAction(c *cli.Context) error {
 
 func bootstrapAction(c *cli.Context) error {
 	machine := c.String("machine")
+	php := c.String("php-version")
+	database := c.String("database")
 	multipass := fmt.Sprintf("%s", c.Context.Value("multipass"))
 
-	cmd := exec.Command(multipass, "delete", machine)
+	// create the machine
+	cmd := exec.Command(
+		multipass,
+		"exec",
+		machine,
+		"--",
+		"sudo",
+		"bash",
+		"/opt/nitro/bootstrap.sh",
+		php,
+		database,
+	)
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
