cfcc177ac8c2c9f3a1702a9880e70eb16b4d74fc
Jason McCallister <jason@craftcms.com>
2020-03-16 09:38:02 -0400

    flip logic
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/command/hosts.go
+++ internal/command/hosts.go
@@ -35,7 +35,7 @@ func beforeHostsAction(c *cli.Context) error {
 	}
 
 	user := os.Getuid()
-	if (user == 0) || (user == -1) {
+	if (user != 0) || (user != -1) {
 		return errors.New("this command requires root/admin privileges")
 	}
 
