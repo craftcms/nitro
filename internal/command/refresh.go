d3fade8d4f09e5f6516ba51934c07401c6f27b82
Jason McCallister <jason@craftcms.com>
2020-03-19 17:39:12 -0400

    adds refresh command, closes #24
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/command/refresh.go
+++ internal/command/refresh.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"fmt"
+
 	"github.com/urfave/cli/v2"
 	"gopkg.in/yaml.v2"
 )
@@ -29,11 +31,25 @@ func refreshAction(c *cli.Context, r Runner) error {
 		return err
 	}
 
+	// make the files in /tmp/opt/
+	for _, dir := range []string{"/tmp/opt/nitro/php", "/tmp/opt/nitro/mariadb", "/tmp/opt/nitro/postgres", "/tmp/opt/nitro/nginx"} {
+		if err := r.Run([]string{"exec", c.String("machine"), "--", "mkdir", "-p", dir}); err != nil {
+			return err
+		}
+	}
+	fmt.Println("created temp directories")
+
+	// grant the ubuntu user and group ownership on /tmp/opt/
+	if err := r.Run([]string{"exec", c.String("machine"), "--", "sudo", "chown", "-R", "ubuntu:ubuntu", "/tmp/opt/nitro"}); err != nil {
+		return err
+	}
+	fmt.Println("change ownership to ubuntu:ubuntu")
+
 	// for each of those, override the path with the content
 	for _, file := range cfg.WriteFiles {
-		// skip the script that does the updates
-		if file.Path == "/opt/nitro/refresh.sh" {
-			continue
+		// create a tmp file to avoid this issue: https://github.com/canonical/multipass/issues/1434
+		if err := r.Run([]string{"exec", c.String("machine"), "--", "touch", "/tmp" + file.Path}); err != nil {
+			return err
 		}
 
 		if err := r.SetInput(file.Content); err != nil {
@@ -43,7 +59,15 @@ func refreshAction(c *cli.Context, r Runner) error {
 		if err := r.Run([]string{"transfer", "-", c.String("machine") + ":/tmp" + file.Path}); err != nil {
 			return err
 		}
+		fmt.Println("transferred file", file.Path)
 	}
+
+	// copy the tmp files
+	fmt.Println("moving files...")
+	if err := r.Run([]string{"exec", c.String("machine"), "--", "sudo", "cp", "-r", "/tmp/opt/nitro", "/opt/nitro"}); err != nil {
+		return err
+	}
+	fmt.Println("refresh complete!")
 
 	return nil
 }
