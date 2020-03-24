ebe60b8d855773db1d2395a748240696e346c789
Jason McCallister <jason@craftcms.com>
2020-03-24 15:24:30 -0400

    add viper and example config file
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- cmd/nitro/main.go
+++ cmd/nitro/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/spf13/viper"
 
 	"github.com/craftcms/nitro/internal/app"
 	"github.com/craftcms/nitro/internal/command"
@@ -14,7 +17,31 @@ func run(args []string) {
 	}
 }
 
+func init() {
+	v := viper.New()
+	v.SetConfigName("nitro")
+	v.SetConfigType("yml")
+	v.AddConfigPath(".")
+
+	// set defaults
+	v.SetDefault("name", "nitro-dev")
+	v.SetDefault("system.php", "7.4")
+	v.SetDefault("system.cpus", "4")
+	v.SetDefault("system.memory", "2G")
+	v.SetDefault("system.disk", "20G")
+
+	err := v.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+}
+
 func main() {
-	// todo check for a config file and append to args as flags
-	run(os.Args)
+
+	args := os.Args[:1]
+	log.Println("args:", args)
+	args = append(args, "--machine="+viper.GetString("name"))
+	log.Println("appended:", args)
+	log.Println("after:", os.Args[len(os.Args)-1:len(os.Args)])
+	//run(args)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/craftcms/nitro
 go 1.13
 
 require (
+	github.com/spf13/viper v1.6.2
 	github.com/txn2/txeh v1.3.0
 	github.com/urfave/cli/v2 v2.1.1
-	gopkg.in/yaml.v2 v2.2.2
+	gopkg.in/yaml.v2 v2.2.4
 )
