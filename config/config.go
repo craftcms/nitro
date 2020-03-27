5e5f6c5e01e7e8120407745d681e2d0f7bba20ed
Jason McCallister <jason@craftcms.com>
2020-03-27 11:04:13 -0400

    wip config package
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"flag"
+
+	"github.com/spf13/viper"
+)
 
 // ReadFile takes a file and default arguments. The override arguments are then passed in as
 // from the cli as flags or global defaults such as CPU, Memory, and Disk.
@@ -21,4 +25,24 @@ func ReadFile(file string, overrides map[string]interface{}) (*viper.Viper, erro
 	err := v.ReadInConfig()
 
 	return v, err
+}
+
+func ReadFileWithFlags(file string, overrides *flag.FlagSet) *viper.Viper {
+	v := viper.New()
+
+	if overrides != nil {
+		overrides.VisitAll(func(f *flag.Flag) {
+			v.SetDefault(f.Name, f.Value)
+		})
+	}
+
+	v.SetConfigFile(file)
+	v.SetConfigType("yaml")
+	v.AddConfigPath("$HOME/.nitro/")
+
+	// explicitly ignore the reading of the config. This throws an error when the file is not found
+	// and nitro does not require a file on init.
+	_ = v.ReadInConfig()
+
+	return v
 }
