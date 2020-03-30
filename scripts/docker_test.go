22e7d45dfed79360227be22219b6056b65d1e906
Jason McCallister <jason@craftcms.com>
2020-03-30 10:32:14 -0400

    update tests for docker
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- scripts/docker_test.go
+++ scripts/docker_test.go
@@ -23,13 +23,22 @@ func TestRunDatabase(t *testing.T) {
 				engine:  "postgres",
 				version: "11.5",
 			},
-			want: []string{"exec", "some-name", "--", "docker", "run", "-d", "--restart=always", "postgres" + ":" + "11.5", "-p", "5432:5432", "-e", "POSTGRES_PASSWORD=nitro", "-e", "POSTGRES_USER=nitro", "-e", "POSTGRES_DB=nitro"},
+			want: []string{"exec", "some-name", "--", "docker", "run", "-d", "--restart=always", "-p", "5432:5432", "-e", "POSTGRES_PASSWORD=nitro", "-e", "POSTGRES_USER=nitro", "-e", "POSTGRES_DB=nitro", "postgres" + ":" + "11.5"},
+		},
+		{
+			name: "can run mysql",
+			args: args{
+				name:    "some-name",
+				engine:  "mysql",
+				version: "5.7",
+			},
+			want: []string{"exec", "some-name", "--", "docker", "run", "-d", "--restart=always", "-p", "3306:3306", "-e", "MYSQL_ROOT_PASSWORD=nitro", "-e", "MYSQL_DATABASE=nitro", "-e", "MYSQL_USER=nitro", "-e", "MYSQL_PASSWORD=nitro", "mysql" + ":" + "5.7"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := DockerRunDatabase(tt.args.name, tt.args.engine, tt.args.version); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("DockerRunDatabase() = %v, want %v", got, tt.want)
+				t.Errorf("DockerRunDatabase() = \n%v, want \n%v", got, tt.want)
 			}
 		})
 	}
