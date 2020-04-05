42f6a1b1d270c930f1fd65f310b4f4a71aca6a7c
Jason McCallister <jason@craftcms.com>
2020-04-05 16:10:47 -0400

    rename the database version func
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/validate/validate_test.go
+++ internal/validate/validate_test.go
@@ -328,8 +328,8 @@ func TestDatabaseVersion(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := DatabaseVersion(tt.args.e, tt.args.v); (err != nil) != tt.wantErr {
-				t.Errorf("DatabaseVersion() error = %v, wantErr %v", err, tt.wantErr)
+			if err := DatabaseEngineAndVersion(tt.args.e, tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("DatabaseEngineAndVersion() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
