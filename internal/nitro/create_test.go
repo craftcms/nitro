96baa2d13d3d27646b2e1e2aaf0df525229061b5
Jason McCallister <jason@craftcms.com>
2020-04-07 10:55:43 -0400

    add blackfire agent to php install
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/nitro/create_test.go
+++ internal/nitro/create_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestInit(t *testing.T) {
+func TestCreate(t *testing.T) {
 	type args struct {
 		name    string
 		cpus    string
@@ -43,7 +43,7 @@ func TestInit(t *testing.T) {
 					Machine:   "thisname",
 					Type:      "exec",
 					Chainable: true,
-					Args:      []string{"thisname", "--", "sudo", "apt", "install", "-y", "php7.4", "php7.4-mbstring", "php7.4-cli", "php7.4-curl", "php7.4-fpm", "php7.4-gd", "php7.4-intl", "php7.4-json", "php7.4-mysql", "php7.4-opcache", "php7.4-pgsql", "php7.4-zip", "php7.4-xml", "php-xdebug", "php-imagick"},
+					Args:      []string{"thisname", "--", "sudo", "apt", "install", "-y", "php7.4", "php7.4-mbstring", "php7.4-cli", "php7.4-curl", "php7.4-fpm", "php7.4-gd", "php7.4-intl", "php7.4-json", "php7.4-mysql", "php7.4-opcache", "php7.4-pgsql", "php7.4-zip", "php7.4-xml", "php-xdebug", "php-imagick", "blackfire-agent", "blackfire-php"},
 				},
 				{
 					Machine:   "thisname",
@@ -82,7 +82,7 @@ func TestInit(t *testing.T) {
 					Machine:   "thisname",
 					Type:      "exec",
 					Chainable: true,
-					Args:      []string{"thisname", "--", "sudo", "apt", "install", "-y", "php7.3", "php7.3-mbstring", "php7.3-cli", "php7.3-curl", "php7.3-fpm", "php7.3-gd", "php7.3-intl", "php7.3-json", "php7.3-mysql", "php7.3-opcache", "php7.3-pgsql", "php7.3-zip", "php7.3-xml", "php-xdebug", "php-imagick"},
+					Args:      []string{"thisname", "--", "sudo", "apt", "install", "-y", "php7.3", "php7.3-mbstring", "php7.3-cli", "php7.3-curl", "php7.3-fpm", "php7.3-gd", "php7.3-intl", "php7.3-json", "php7.3-mysql", "php7.3-opcache", "php7.3-pgsql", "php7.3-zip", "php7.3-xml", "php-xdebug", "php-imagick", "blackfire-agent", "blackfire-php"},
 				},
 				{
 					Machine:   "thisname",
