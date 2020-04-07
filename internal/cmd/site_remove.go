b78da4213c8905079b8afe451fd551ab82325c3b
Jason McCallister <jason@craftcms.com>
2020-04-07 08:46:20 -0400

    more tests, remove empty cmd
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/site_remove.go
+++ internal/cmd/site_remove.go
@@ -1,18 +1,19 @@
 package cmd
 
 import (
+	"errors"
+
 	"github.com/spf13/cobra"
 
 	"github.com/craftcms/nitro/config"
-	"github.com/craftcms/nitro/internal/nitro"
 )
 
 var siteRemoveCommand = &cobra.Command{
 	Use:   "remove",
 	Short: "Remove a site from machine",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		name := config.GetString("machine", flagMachineName)
-
-		return nitro.Run(nitro.NewMultipassRunner("multipass"), nitro.Empty(name))
+		_ = config.GetString("machine", flagMachineName)
+		return errors.New("not implemented")
+		// return nitro.Run(nitro.NewMultipassRunner("multipass"), nitro.Empty(name))
 	},
 }
---
+++ internal/nitro/destroy_test.go
@@ -0,0 +1,56 @@
+package nitro
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDestroy(t *testing.T) {
+	type args struct {
+		name      string
+		permanent bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Command
+	}{
+		{
+			name: "get command to permanently delete the machine",
+			args: args{
+				name:      "some-machine",
+				permanent: false,
+			},
+			want: []Command{
+				{
+					Machine:   "some-machine",
+					Chainable: false,
+					Type:      "delete",
+					Args:      []string{"some-machine"},
+				},
+			},
+		},
+		{
+			name: "get command to permanently delete the machine",
+			args: args{
+				name:      "some-machine",
+				permanent: true,
+			},
+			want: []Command{
+				{
+					Machine:   "some-machine",
+					Chainable: true,
+					Type:      "delete",
+					Args:      []string{"some-machine", "-p"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Destroy(tt.args.name, tt.args.permanent); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Destroy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
