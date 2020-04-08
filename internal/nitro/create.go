c32cee28e0577fcb000dcbefec992d44ad40c891
Jason McCallister <jason@craftcms.com>
2020-04-08 07:00:25 -0400

    move update to after blackfire install
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/nitro/create.go
+++ internal/nitro/create.go
@@ -44,11 +44,11 @@ runcmd:
   - sudo add-apt-repository -y ppa:ondrej/php
   - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   - sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  - sudo apt-get update -y
   - sudo apt install -y nginx docker-ce docker-ce-cli containerd.io
   - sudo usermod -aG docker ubuntu
   - wget -q -O - https://packages.blackfire.io/gpg.key | sudo apt-key add -
   - echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
+  - sudo apt-get update -y
   - sudo mkdir -p /opt/nitro/volumes/mysql
   - sudo mkdir -p /opt/nitro/volumes/postgres
   - sudo chown -R ubuntu:ubuntu /opt/nitro
