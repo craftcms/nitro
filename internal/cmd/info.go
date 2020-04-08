5e1de4d53c89ec211ab2a95914adb17fd9d42839
Jason McCallister <jason@craftcms.com>
2020-04-08 12:23:15 -0400

    add info action
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/info.go
+++ internal/cmd/info.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/craftcms/nitro/config"
+	"github.com/craftcms/nitro/internal/action"
 	"github.com/craftcms/nitro/internal/nitro"
 )
 
@@ -11,8 +12,13 @@ var infoCommand = &cobra.Command{
 	Use:   "info",
 	Short: "Show machine info",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		name := config.GetString("machine", flagMachineName)
+		name := config.GetString("name", flagMachineName)
 
-		return nitro.Run(nitro.NewMultipassRunner("multipass"), nitro.Info(name))
+		infoAction, err := action.Info(name)
+		if err != nil {
+			return err
+		}
+
+		return nitro.RunAction(nitro.NewMultipassRunner("multipass"), []action.Action{*infoAction})
 	},
 }
