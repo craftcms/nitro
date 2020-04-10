5db933a4bfd34809eafd125fb620f15b575bbd30
Jason McCallister <jason@craftcms.com>
2020-04-10 15:13:25 -0400

    update goreleaser with sha sums
    Signed-off-by: Jason McCallister <jason@craftcms.com>
    
    # Conflicts:
    #	.gitignore

--- hashgen.sh
+++ hashgen.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-for f in ./*; do shasum -a 256 $f > $f.sha256; done
+for folder in ./dist/*; do
+  echo "$folder"
+  if [ -d "$folder" ]; then
+    for f in "$folder"/*; do
+      shasum -a 256 "$f" >>"$f".sha256
+    done
+  fi
+done
