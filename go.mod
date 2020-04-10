44c6709f6f1d50ab5d5a4aa83621cbf9b5c02e3d
Jason McCallister <jason@craftcms.com>
2020-04-10 13:13:59 -0400

    add logs database with interactive ui
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/craftcms/nitro
 go 1.14
 
 require (
+	github.com/manifoldco/promptui v0.7.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.6.2
--- internal/cmd/logs_database.go
+++ internal/cmd/logs_database.go
@@ -2,8 +2,14 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
 
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/craftcms/nitro/config"
+	"github.com/craftcms/nitro/internal/action"
 )
 
 var logsDatabaseCommand = &cobra.Command{
@@ -11,7 +17,38 @@ var logsDatabaseCommand = &cobra.Command{
 	Short:  "Show database logs",
 	Hidden: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return errors.New("not implemented yet")
+		name := config.GetString("name", flagMachineName)
+		if !viper.IsSet("databases") {
+			return errors.New("no databases found in " + viper.ConfigFileUsed())
+		}
+
+		var databases []config.Database
+		if err := viper.UnmarshalKey("databases", &databases); err != nil {
+			return err
+		}
+
+		// TODO abstract this
+		var dbs []string
+		for _, db := range databases {
+			dbs = append(dbs, fmt.Sprintf("%s_%s_%s", db.Engine, db.Version, db.Port))
+		}
+
+		prompt := promptui.Select{
+			Label: "Select database",
+			Items: dbs,
+		}
+
+		_, container, err := prompt.Run()
+		if err != nil {
+			return err
+		}
+
+		dockerLogsAction, err := action.LogsDocker(name, container)
+		if err != nil {
+			return err
+		}
+
+		return action.Run(action.NewMultipassRunner("multipass"), []action.Action{*dockerLogsAction})
 	},
 }
 
