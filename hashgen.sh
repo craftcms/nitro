a7cc39e16eb3d1346916771f4d96fb783c4c4b58
Jason McCallister <jason@craftcms.com>
2020-04-10 15:23:44 -0400

    fix folder names
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- hashgen.sh
+++ hashgen.sh
@@ -4,7 +4,7 @@ for folder in ./dist/*; do
   echo "$folder"
   if [ -d "$folder" ]; then
     for f in "$folder"/*; do
-      shasum -a 256 "$f" >>"$f".sha256
+      shasum -a 256 "$f" > "$folder".sha256
     done
   fi
 done
