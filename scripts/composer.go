40bf5bf3e867713a172447dde3868db0b5f24c8d
Jason McCallister <jason@craftcms.com>
2020-04-13 08:46:06 -0400

    remove scripts package
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- scripts/composer.go
+++
@@ -1,7 +0,0 @@
-package scripts
-
-// InstallComposer returns a script that will install the latest composer. It might be a good idea to provide a specific version?
-// to prevent scenarios like this: https://github.com/composer/composer/issues/8710
-func InstallComposer() string {
-	return `php -r "readfile('http://getcomposer.org/installer')" | sudo php -- --install-dir=/usr/bin/ --filename=composer`
-}
