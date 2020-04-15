64a22b0b5b2b104f8cd3b74f49d1b50bef7d3f25
Jason McCallister <jason@craftcms.com>
2020-04-15 14:14:53 -0400

    remove Version and use cmd.Version
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -6,16 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	Version = "0.0.0"
+var versionCommand = &cobra.Command{
+	Use:   "version",
+	Short: "View Nitro version",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Printf("Nitro version %s\n", cmd.Version)
 
-	versionCommand = &cobra.Command{
-		Use:   "version",
-		Short: "View Nitro version",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Printf("Nitro version %s\n", Version)
-
-			return nil
-		},
-	}
-)
+		return nil
+	},
+}
