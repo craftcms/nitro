6223e0a1830b950b1559bf07c5f730173b5f879e
Jason McCallister <jason@craftcms.com>
2020-04-15 15:52:07 -0400

    wip hack package
    Signed-off-by: Jason McCallister <jason@craftcms.com>

---
+++ internal/hack/apply_diff.go
@@ -0,0 +1,10 @@
+package hack
+
+import (
+	"github.com/craftcms/nitro/config"
+	"github.com/craftcms/nitro/internal/nitro"
+)
+
+func Diff(name string, orig config.Config, new config.Config) *[]nitro.Action {
+
+}
---
+++ internal/hack/prompt_machine.go
@@ -0,0 +1,8 @@
+package hack
+
+// TODO create apply command
+// TODO prompt for config file creation if empty
+
+// TODO simplify logs
+// TODO move builds to buddy
+// TODO automatic hosts editing
