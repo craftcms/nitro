24a4961257d4d49a3bb51ecd98d4c263d7fc8796
Jason McCallister <jason@craftcms.com>
2020-04-16 15:27:23 -0400

    add latest version check to version command
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/spf13/cobra"
+
+	"github.com/craftcms/nitro/internal/hack"
 )
 
 var (
@@ -13,9 +16,17 @@ var (
 		Use:   "version",
 		Short: "View Nitro version",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Printf("nitro version %s\n", Version)
+			// show current version
+			fmt.Printf("nitro %s\n", Version)
 			fmt.Println("")
-			fmt.Println("visit https://github.com/craftcms/nitro for more information")
+
+			latest, err := hack.GetLatestVersion(http.DefaultClient, "https://api.github.com/repos/pixelandtonic/nitro/releases")
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("The latest version of nitro is", latest)
+			fmt.Println("Visit https://github.com/craftcms/nitro for more details or \nrun `nitro self-update` to perform an upgrade")
 
 			return nil
 		},
