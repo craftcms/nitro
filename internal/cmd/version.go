bbf9cf1b4a0c360cfb8d56b18217770637e6fc84
Jason McCallister <jason@craftcms.com>
2020-04-16 15:00:18 -0400

    syntax
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -6,21 +6,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "0.0.0"
+var (
+	Version = "0.0.0"
+
+	versionCommand = &cobra.Command{
+		Use:   "version",
+		Short: "View Nitro version",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Printf("nitro version %s\n", Version)
+			fmt.Println("")
+			fmt.Println("visit https://github.com/craftcms/nitro for more information")
+
+			return nil
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(versionCommand)
-}
-
-var versionCommand = &cobra.Command{
-	Use:   "version",
-	Short: "View Nitro version",
-	Long:  ``,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Printf("nitro version %s\n", Version)
-		fmt.Println("")
-		fmt.Println("visit https://github.com/craftcms/nitro for more information")
-
-		return nil
-	},
 }
