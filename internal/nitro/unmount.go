f60bcb2bca782fbe9be1684b182a6319efbf8217
Jason McCallister <jason@craftcms.com>
2020-04-16 11:05:31 -0400

    add unmountdir action
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/nitro/unmount.go
+++ internal/nitro/unmount.go
@@ -13,3 +13,15 @@ func Unmount(name, site string) (*Action, error) {
 		Args:       []string{"umount", name + ":/app/sites/" + site},
 	}, nil
 }
+
+func UnmountDir(name, target string) (*Action, error) {
+	if err := validate.MachineName(name); err != nil {
+		return nil, err
+	}
+
+	return &Action{
+		Type:       "umount",
+		UseSyscall: false,
+		Args:       []string{"umount", name + ":" + target},
+	}, nil
+}
