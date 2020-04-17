ea8f9c7d94b7dfa25aabcbfeb29a2e7333e03b5c
Jason McCallister <jason@craftcms.com>
2020-04-17 13:50:31 -0400

    this is broken, but file saving is as well
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- config/config_save_test.go
+++ config/config_save_test.go
@@ -63,12 +63,13 @@ func TestCanSaveConfigProperly(t *testing.T) {
 	// double check the golden file
 	goldenCfgFile := getConfigFile(t, "testdata/configs/golden-full.yaml")
 	// make sure the mounts are the same
+	// TODO reflect.DeepEqual verifies memory
 	if !reflect.DeepEqual(goldenCfgFile.Mounts, modifiedCfgFile.Mounts) {
-		t.Errorf("expected configs mounts to be the same, got \n%v \nwant: \n%v", goldenCfgFile.Mounts, modifiedCfgFile.Mounts)
+		t.Errorf("expected configs mounts to be the same, got \n%v \nwant: \n%v", modifiedCfgFile.Mounts, goldenCfgFile.Mounts)
 	}
 	// make sure the sites are the same
 	if !reflect.DeepEqual(goldenCfgFile.Sites, modifiedCfgFile.Sites) {
-		t.Errorf("expected configs sites to be the same, got \n%v \nwant: \n%v", goldenCfgFile.Sites, modifiedCfgFile.Sites)
+		t.Errorf("expected configs sites to be the same, got \n%p \nwant: \n%p", &modifiedCfgFile.Sites, &goldenCfgFile.Sites)
 	}
 	// make sure the entire config is the same
 	if !reflect.DeepEqual(goldenCfgFile, modifiedCfgFile) {
