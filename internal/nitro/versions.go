8013a8f9fbab672a339050851d8dd4e2b0a90a38
Jason McCallister <jason@craftcms.com>
2020-04-20 12:57:04 -0400

    add more versions to DBVersions
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/nitro/versions.go
+++ internal/nitro/versions.go
@@ -4,7 +4,7 @@ var (
 	PHPVersions = []string{"7.4", "7.3", "7.2", "7.1", "7.0"}
 	DBEngines   = []string{"mysql", "postgres"}
 	DBVersions  = map[string][]string{
-		"mysql":    {"8.0", "5.7", "5.6"},
-		"postgres": {"12", "11", "10", "9"},
+		"mysql":    {"8.0", "5.8", "5.7", "5.6", "5"},
+		"postgres": {"12", "12.2", "11.7", "11", "10.12", "10", "9.6", "9.6", "9"},
 	}
 )
