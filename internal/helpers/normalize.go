70d062e3171b408f5ca30dc196381954e33f629e
Jason McCallister <jason@craftcms.com>
2020-04-20 17:32:09 -0400

    add tests and helper for path normalizing
    Signed-off-by: Jason McCallister <jason@craftcms.com>

---
+++ internal/helpers/normalize.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// NormalizePath is responsible for taking a path, relative
+// or otherwise, and returning the name of the file,
+// the absolute path, and an error if not found.
+func NormalizePath(path, home string) (string, string, error) {
+	p := strings.Split(path, string(os.PathSeparator))
+
+	if strings.Contains(p[0], "~") {p[0] = home
+		path = strings.Join(p, string(os.PathSeparator))
+	}
+
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return "", "", err
+	}
+
+	filename := p[len(p)-1]
+
+	return filename, absPath, nil
+}
