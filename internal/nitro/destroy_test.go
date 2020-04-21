26daa877019243382b23f0d7570cbf39dbced7fc
Jason McCallister <jason@craftcms.com>
2020-04-21 07:27:23 -0400

    update destroy to always be permanent
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/nitro/destroy_test.go
+++ internal/nitro/destroy_test.go
@@ -17,22 +17,9 @@ func TestDestroy(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "can destroy a machine",
-			args: args{
-				name:  "notpermanent",
-				force: false,
-			},
-			want: &Action{
-				Type:       "delete",
-				UseSyscall: false,
-				Args:       []string{"delete", "notpermanent"},
-			},
-		},
-		{
 			name: "can destroy a machine permanently",
 			args: args{
 				name:  "ispermanent",
-				force: true,
 			},
 			want: &Action{
 				Type:       "delete",
@@ -43,7 +30,7 @@ func TestDestroy(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Destroy(tt.args.name, tt.args.force)
+			got, err := Destroy(tt.args.name)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Destroy() error = %v, wantErr %v", err, tt.wantErr)
 				return
