a67cc1adcec6477421f55629e14de0237696af72
Jason McCallister <jason@craftcms.com>
2020-04-28 16:32:58 -0400

    new line
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/add.go
+++ internal/cmd/add.go
@@ -91,7 +91,7 @@ var addCommand = &cobra.Command{
 			}
 		}
 
-		fmt.Printf("%s has been added to nitro.yaml", hostname)
+		fmt.Printf("%s has been added to nitro.yaml\n", hostname)
 
 		applyChanges, err := prompt.Verify(ui, "Apply changes from config now?", "y")
 		if err != nil {
