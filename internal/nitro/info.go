4f5f8dd3528823f4096a75f0594ea68f9b8c683e
Jason McCallister <jason@craftcms.com>
2020-04-29 11:47:50 -0400

    semantics
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/nitro/info.go
+++ internal/nitro/info.go
@@ -3,14 +3,14 @@ package nitro
 import "errors"
 
 // Info will display the machine information based on a name
-func Info(name string) (*Action, error) {
-	if name == "" {
+func Info(machine string) (*Action, error) {
+	if machine == "" {
 		return nil, errors.New("missing machine name")
 	}
 
 	return &Action{
 		Type:       "info",
 		UseSyscall: false,
-		Args:       []string{"info", name},
+		Args:       []string{"info", machine},
 	}, nil
 }
