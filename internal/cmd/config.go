a0b5597b90c98efe6e85e07445b1f43771eaa222
Jason McCallister <jason@craftcms.com>
2020-04-29 17:08:36 -0400

    wait for mysql to be ready before continuing
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -60,6 +60,7 @@ write_files:
       fi
 
       if [ "$engine" == "mysql" ]; then
+          docker exec "$container" bash -c "while ! mysqladmin ping -h 127.0.0.1 -uroot -pnitro; do echo 'waiting...'; sleep 1; done"
           docker exec "$container" mysql -uroot -pnitro -e "GRANT ALL ON *.* TO 'nitro'@'%';"
           docker exec "$container" mysql -uroot -pnitro -e "FLUSH PRIVILEGES;"
           echo "setting root permissions on user nitro"
