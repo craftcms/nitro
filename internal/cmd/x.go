56d34f5ffd615b3223a700cddf40b25e204d0b77
Jason McCallister <themccallister@gmail.com>
2020-05-10 17:59:50 -0400

    docs for the wip

--- internal/cmd/x.go
+++ internal/cmd/x.go
@@ -15,6 +15,11 @@ var (
 
 			return nil
 
+			// TODO follow the same "sudo" process but use the runas syntax
+			// from https://www.windows-commandline.com/windows-runas-command-prompt/
+
+			// runas /user:"Jason McCallister":administrator nitro.exe
+
 			//args = append(args, "--machine=nitro-dev", "hosts")
 			//if err := runas.Elevated("nitro-dev", args); err != nil {
 			//	return err
