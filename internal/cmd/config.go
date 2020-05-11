c80bbcce7c5090911a289936dc26113dfd8d1cda
Jason McCallister <jason@craftcms.com>
2020-05-11 11:58:40 -0400

    Set DB_USER and DB_PASSWORD in env - resolves #119
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -117,6 +117,8 @@ runcmd:
   - sudo add-apt-repository --no-update -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   - wget -q -O - https://packages.blackfire.io/gpg.key | sudo apt-key add -
   - echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
+  - echo "DB_USER=nitro" >> /etc/environment
+  - echo "DB_PASSWORD=nitro" >> /etc/environment
   - apt-get update -y
   - apt install -y nginx docker-ce docker-ce-cli containerd.io
   - usermod -aG docker ubuntu
