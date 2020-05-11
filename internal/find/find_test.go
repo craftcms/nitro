ff0206c5032ec72140ff7a7db2cd75f8e6be1273
Jason McCallister <jason@craftcms.com>
2020-05-10 21:15:15 -0400

    more tests
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/find/find_test.go
+++ internal/find/find_test.go
@@ -40,11 +40,22 @@ func TestMounts(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "sources with a space still return a mount",
-			args: args{name: "nitro-dev", b: []byte("Name,State,Ipv4,Ipv6,Release,Image hash,Image release,Load,Disk usage,Disk total,Memory usage,Memory total,Mounts\nnitro-dev,Running,192.168.64.21,,Ubuntu 18.04.4 LTS,3b2e3aaaebf2bc364da70fbc7e9619a7c0bb847932496a1903cd4913cf9b1a26,18.04 LTS,0.01 0.21 0.22,2544762880,41442029568,393465856,4136783872,/Users/jason mccallister/go/src/github.com/craftcms/nitro/production-site => /nitro/sites/production-site;/Users/jason mccallister/go/src/github.com/craftcms/nitro/demo-site => /nitro/sites/demo-site;\n")},
+			name: "sources with a space in the path still return a mount",
+			args: args{name: "nitro-dev", b: []byte("Name,State,Ipv4,Ipv6,Release,Image hash,Image release,Load,Disk usage,Disk total,Memory usage,Memory total,Mounts\nnitro-dev,Running,192.168.64.21,,Ubuntu 18.04.4 LTS,3b2e3aaaebf2bc364da70fbc7e9619a7c0bb847932496a1903cd4913cf9b1a26,18.04 LTS,0.01 0.21 0.22,2544762880,41442029568,393465856,4136783872,/Users/jason mccallister/go/src/github.com/craftcms/nitro/production-site => /nitro/sites/production-site;\n")},
 			want: []config.Mount{
 				{
 					Source: "/Users/jason mccallister/go/src/github.com/craftcms/nitro/production-site",
+					Dest:   "/nitro/sites/production-site",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "sources with a space at the end return a mount without the space",
+			args: args{name: "nitro-dev", b: []byte("Name,State,Ipv4,Ipv6,Release,Image hash,Image release,Load,Disk usage,Disk total,Memory usage,Memory total,Mounts\nnitro-dev,Running,192.168.64.21,,Ubuntu 18.04.4 LTS,3b2e3aaaebf2bc364da70fbc7e9619a7c0bb847932496a1903cd4913cf9b1a26,18.04 LTS,0.01 0.21 0.22,2544762880,41442029568,393465856,4136783872,/Users/jasonmccallister/go/src/github.com/craftcms/nitro/production-site  => /nitro/sites/production-site ;\n")},
+			want: []config.Mount{
+				{
+					Source: "/Users/jasonmccallister/go/src/github.com/craftcms/nitro/production-site",
 					Dest:   "/nitro/sites/production-site",
 				},
 			},
@@ -58,7 +69,12 @@ func TestMounts(t *testing.T) {
 				t.Errorf("Mounts() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+
 			if tt.want != nil {
+				if len(tt.want) != len(got) {
+					t.Errorf("expected the length to match, got %d but want %d", len(got), len(tt.want))
+				}
+
 				for i, mount := range tt.want {
 					if !reflect.DeepEqual(mount, got[i]) {
 						t.Errorf("Mounts() got = \n%v, \nwant \n%v", got[i], tt.want[i])
