bf43be363a80ceb3d6e714d9d75bb17cc78e8629
Jason McCallister <jason@craftcms.com>
2020-05-13 12:04:53 -0400

    cleanup the scripts pkg
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/scripts/scripts.go
+++ internal/scripts/scripts.go
@@ -5,11 +5,36 @@ import (
 	"strings"
 )
 
-// Run is a process that takes the path to multipass and args that
-// should be sent to the multipass CLI and return the cmd output
-// or an error. Output will auto trim spaces (e.g. new lines)
-func Run(multipass string, args []string) (string, error) {
-	cmd := exec.Command(multipass, args...)
+const (
+	FmtNginxSiteAvailable = `if test -f '/etc/nginx/sites-available/%s'; then echo 'exists'; fi`
+	FmtNginxSiteEnabled   = `if test -f '/etc/nginx/sites-enabled/%s'; then echo 'exists'; fi`
+	FmtNginxSiteWebroot   = `grep "root " /etc/nginx/sites-available/%s | while read -r line; do echo "$line"; done`
+)
+
+type Script struct {
+	path    string
+	machine string
+}
+
+// New will return a new Script struct that
+// contains the path to multipass and
+// the name of the machine
+func New(multipass, machine string) *Script {
+	return &Script{
+		path:    multipass,
+		machine: machine,
+	}
+}
+
+// Run is used to make running scripts on a nitro machine
+// a lot easier, using New will store the path to the
+// nitro path and machine name. Run will then run
+// the script on the machine and
+func (s Script) Run(arg ...string) (string, error) {
+	args := []string{"exec", s.machine, "--", "bash", "-c"}
+	args = append(args, arg...)
+
+	cmd := exec.Command(s.path, args...)
 
 	bytes, err := cmd.CombinedOutput()
 	if err != nil {
