66fbece2ad06c3f172749ab7cc785b700823f96e
Jason McCallister <jason@craftcms.com>
2020-05-23 13:07:14 -0400

    wip install postgres command
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/install.go
+++ internal/cmd/install.go
@@ -7,12 +7,12 @@ import (
 var installCommand = &cobra.Command{
 	Use:       "install",
 	Short:     "Install software",
-	ValidArgs: []string{"composer", "mailhog"},
+	ValidArgs: []string{"composer", "mailhog", "postgres"},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return cmd.Help()
 	},
 }
 
 func init() {
-	installCommand.AddCommand(mailhogCommand, composerCommand)
+	installCommand.AddCommand(mailhogCommand, composerCommand, postgresCommand)
 }
