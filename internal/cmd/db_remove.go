a8fdd8ffc325cf7078f1233671264f3b2e53716f
Jason McCallister <jason@craftcms.com>
2020-05-26 16:25:07 -0700

    remove database, not engine
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/db_remove.go
+++ internal/cmd/db_remove.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os/exec"
+	"strings"
 
 	"github.com/pixelandtonic/prompt"
 	"github.com/spf13/cobra"
@@ -48,7 +49,7 @@ var dbRemoveCommand = &cobra.Command{
 		case 1:
 			container = containers[0]
 		default:
-			container, _, err = p.Select("Select database engine to remove", containers, &prompt.SelectOptions{
+			container, _, err = p.Select("Select the database engine", containers, &prompt.SelectOptions{
 				Default: 1,
 			})
 			if err != nil {
@@ -56,9 +57,42 @@ var dbRemoveCommand = &cobra.Command{
 			}
 		}
 
-		remove, err := p.Confirm("Are you sure you want to permanently remove the database engine "+container, &prompt.InputOptions{
-			Default:   "no",
-			Validator: nil,
+		// get all of the databases in the engine
+		var dbs []string
+		switch strings.Contains(container, "mysql") {
+		case false:
+			if output, err := script.Run(false, fmt.Sprintf(scripts.FmtDockerPostgresShowAllDatabases, container)); err == nil {
+				sp := strings.Split(output, "\n")
+				for i, d := range sp {
+					if i == 0 || i == 1 || i == len(sp) || strings.Contains(d, "rows)") {
+						continue
+					}
+
+					dbs = append(dbs, strings.TrimSpace(d))
+				}
+			}
+		default:
+			if output, err := script.Run(false, fmt.Sprintf(scripts.FmtDockerMysqlShowAllDatabases, container)); err == nil {
+				for _, db := range strings.Split(output, "\n") {
+					// ignore the system defaults
+					if db == "Database" || db == "information_schema" || db == "performance_schema" || db == "sys" || strings.Contains(db, "password on the command line") || db == "mysql" {
+						continue
+					}
+					dbs = append(dbs, db)
+				}
+			}
+		}
+
+		// ask the user which database to remove
+		database, _, err := p.Select("Select database to remove", dbs, &prompt.SelectOptions{Default: 1})
+		if err != nil {
+			return err
+		}
+
+		// make sure the user wants to do this
+		remove, err := p.Confirm(fmt.Sprintf("Are you sure you want to permanently remove the database %q", database), &prompt.InputOptions{
+			Default:            "no",
+			Validator:          nil,
 			AppendQuestionMark: true,
 		})
 		if err != nil {
@@ -66,21 +100,26 @@ var dbRemoveCommand = &cobra.Command{
 		}
 
 		if remove {
-			_, err = script.Run(false, fmt.Sprintf(scripts.FmtDockerRemoveContainer, container))
-			if err != nil {
-				return err
+			switch strings.Contains(container, "mysql") {
+			case false:
+				// its postgres so remove the db
+				if output, err := script.Run(false, fmt.Sprintf(`docker exec -i %s psql --username nitro -c "DROP DATABASE IF EXISTS %s;"`, container, database)); err != nil {
+					fmt.Println(output)
+					return err
+				}
+			default:
+				// its mysql, drop the db
+				if output, err := script.Run(false, fmt.Sprintf(`docker exec -i %s mysql -unitro -pnitro -e "DROP DATABASE IF EXISTS %s;"`, container, database)); err != nil {
+					fmt.Println(output)
+					return err
+				}
 			}
 
-			_, err = script.Run(false, fmt.Sprintf(scripts.FmtDockerRemoveVolume, container))
-			if err != nil {
-				return err
-			}
-
-			fmt.Println("Removed database engine ", container)
+			fmt.Println("Removed database", database)
 			return nil
 		}
 
-		fmt.Println("There was a problem removing the database", container)
+		fmt.Println("There was a problem removing the database", database)
 
 		return nil
 	},
