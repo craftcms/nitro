aa579b3a7cc158fbf6f5e0c38fbb2abc77a07da3
Jason McCallister <jason@craftcms.com>
2020-05-26 06:52:40 -0700

    semantics
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/suggest/suggest.go
+++ internal/suggest/suggest.go
@@ -2,11 +2,11 @@ package suggest
 
 // NumberOfCPUs will suggest the number of CPUs a
 // machine should be created with based on the
-// current systems CPU count at runtime.
+// current systems CPU count at runtime
 func NumberOfCPUs(num int) string {
 	// Go counts the number of logical CPUs with runtime.NumCPU()
 	// we are only concerned about low end CPUs where performance
-	// is really impacted.
+	// is really impacted
 	switch num {
 	case 4:
 		return "2"
