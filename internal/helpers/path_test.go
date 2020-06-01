6f92c5f9ed21feb783b18cb926f68a4d8bbeed62
Jason McCallister <jason@craftcms.com>
2020-06-01 08:09:13 -0700

    add tests and cleanup GetDirectoryArg
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/helpers/path_test.go
+++ internal/helpers/path_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/mitchellh/go-homedir"
 )
 
 func TestParentPathName(t *testing.T) {
@@ -79,6 +81,80 @@ func TestGetDirectoryArg(t *testing.T) {
 			}
 			if got1 != tt.absolutePath {
 				t.Errorf("GetDirectoryArg() got1 = %v, want %v", got1, tt.absolutePath)
+			}
+		})
+	}
+}
+
+func TestGetDirectoryArg1(t *testing.T) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	home, err := homedir.Dir()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		args []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		want1   string
+		wantErr bool
+	}{
+		{
+			name:    "no arguments returns the directory name and full path",
+			args:    args{args: nil},
+			want:    "helpers",
+			want1:   cwd,
+			wantErr: false,
+		},
+		{
+			name:    "passing an argument with a slash gets the right folder",
+			args:    args{[]string{"testdata/existing-dir"}},
+			want:    "existing-dir",
+			want1:   cwd + "/testdata/existing-dir",
+			wantErr: false,
+		},
+		{
+			name:    "passing an argument gets the right folder",
+			args:    args{[]string{"testdata"}},
+			want:    "testdata",
+			want1:   cwd + "/testdata",
+			wantErr: false,
+		},
+		{
+			name:    "passing an argument with a tilde gets the right folder",
+			args:    args{[]string{"~/testdata"}},
+			want:    "testdata",
+			want1:   home + "/testdata",
+			wantErr: false,
+		},
+		{
+			name:    "passing an argument with a period gets the right folder",
+			args:    args{[]string{"./testdata"}},
+			want:    "testdata",
+			want1:   cwd + "/testdata",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := GetDirectoryArg(tt.args.args)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetDirectoryArg() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetDirectoryArg() \ngot = \n%v, \nwant \n%v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("GetDirectoryArg() \ngot1 = \n%v, \nwant \n%v", got1, tt.want1)
 			}
 		})
 	}
