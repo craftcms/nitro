3f12b05cba9b8504a89ba570748422aea117fe03
Jason McCallister <jason@craftcms.com>
2020-07-13 22:41:10 -0400

    Add tests for spy command and args
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/api/services_php_fpm_test.go
+++ internal/api/services_php_fpm_test.go
@@ -1,10 +1,12 @@
 package api
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -15,9 +17,12 @@ func Test_server_handlePhpFpmService(t *testing.T) {
 		logger *log.Logger
 	}
 	tests := []struct {
-		name       string
-		fields     fields
-		statusCode int
+		name        string
+		fields      fields
+		body        io.Reader
+		statusCode  int
+		wantCommand string
+		wantArgs    []string
 	}{
 		{
 			name: "testing request validation",
@@ -27,24 +32,46 @@ func Test_server_handlePhpFpmService(t *testing.T) {
 			},
 			statusCode: http.StatusUnprocessableEntity,
 		},
+		{
+			name: "testing valid requests call correct actions",
+			fields: fields{
+				router: http.NewServeMux(),
+				logger: log.New(ioutil.Discard, "testing", 0),
+			},
+			body:        strings.NewReader(`{"action":"restart", "version":"7.4"}`),
+			wantCommand: "service",
+			wantArgs:    []string{"php7.4-fpm", "restart"},
+			statusCode:  http.StatusOK,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			spy := &spyServiceRunner{}
 			srv := &server{
 				router:  tt.fields.router,
 				logger:  tt.fields.logger,
-				service: spyServiceRunner{},
+				service: spy,
 			}
 
 			srv.Routes()
 
-			req := httptest.NewRequest(http.MethodPost, "/v1/services/php-fpm", strings.NewReader(""))
+			req := httptest.NewRequest(http.MethodPost, "/v1/services/php-fpm", tt.body)
 			w := httptest.NewRecorder()
 
 			srv.ServeHTTP(w, req)
 
 			if w.Result().StatusCode != tt.statusCode {
 				t.Errorf("expected status code %v, got %v instead", tt.statusCode, w.Result().StatusCode)
+			}
+			if tt.wantCommand != "" {
+				if spy.Command != tt.wantCommand {
+					t.Errorf("wanted the command %v, got %v instead", tt.wantCommand, spy.Command)
+				}
+			}
+			if len(tt.wantArgs) > 0 {
+				if !reflect.DeepEqual(spy.Args, tt.wantArgs) {
+					t.Errorf("expected the args to be the same")
+				}
 			}
 		})
 	}
@@ -55,7 +82,7 @@ type spyServiceRunner struct {
 	Args    []string
 }
 
-func (r spyServiceRunner) Run(command string, args []string) ([]byte, error) {
+func (r *spyServiceRunner) Run(command string, args []string) ([]byte, error) {
 	r.Command = command
 	r.Args = args
 
