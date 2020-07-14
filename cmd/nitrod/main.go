3d43e8956455a31ef23d176ee8458776e532bd18
Jason McCallister <jason@craftcms.com>
2020-07-13 22:15:03 -0400

    clean up
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- cmd/nitrod/main.go
+++ cmd/nitrod/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"flag"
-	"github.com/craftcms/nitro/internal/api"
 	"log"
 	"net/http"
+
+	"github.com/craftcms/nitro/internal/api"
 )
 
 func main() {
 	port := flag.String("port", "9999", "which port the nitro API should listen on")
-
 	flag.Parse()
 
 	srv := api.New()
