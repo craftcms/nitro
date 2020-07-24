fd50e95de205766b424d8e222c57b1316698f805
Jason McCallister <jason@craftcms.com>
2020-07-24 18:33:02 -0400

    clean up
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/api/iniset.go
+++ internal/api/iniset.go
@@ -11,37 +11,45 @@ import (
 
 func (s *NitrodService) PhpIniSettings(ctx context.Context, request *ChangePhpIniSettingRequest) (*ServiceResponse, error) {
 	var setting string
+	value := request.GetValue()
+	version := request.GetVersion()
 
+	// validate the php version
+	if err := validate.PHPVersion(version); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+
+	// validate the setting and value
 	switch request.GetSetting() {
 	case PhpIniSetting_MAX_EXECUTION_TIME:
-		if err := validate.MaxExecutionTime(request.GetValue()); err != nil {
+		if err := validate.MaxExecutionTime(value); err != nil {
 			return nil, status.Errorf(codes.InvalidArgument, err.Error())
 		}
 		setting = "max_execution_time"
 	case PhpIniSetting_MAX_INPUT_VARS:
-		if err := validate.MaxInputVars(request.GetValue()); err != nil {
+		if err := validate.MaxInputVars(value); err != nil {
 			return nil, status.Errorf(codes.InvalidArgument, err.Error())
 		}
 		setting = "max_input_vars"
 	default:
-		msg := "changing " + PhpIniSetting_name[int32(request.GetSetting())] + " setting is not authorized"
+		msg := "changing " + PhpIniSetting_name[int32(request.GetSetting())] + " is not authorized"
 		s.logger.Println(msg)
 		return nil, status.Errorf(codes.InvalidArgument, msg)
 	}
 
 	// change setting using sed
-	if output, err := s.command.Run("sed", []string{"-i", "s|" + setting + "|" + setting + " = " + request.GetValue() + "|g", "/etc/php/" + request.GetVersion() + "/fpm/php.ini"}); err != nil {
+	if output, err := s.command.Run("sed", []string{"-i", "s|" + setting + "|" + setting + " = " + value + "|g", "/etc/php/" + version + "/fpm/php.ini"}); err != nil {
 		s.logger.Println("error changing ini setting, error:", err)
 		s.logger.Println("output:", string(output))
 		return nil, status.Errorf(codes.Unknown, string(output))
 	}
 
 	// restart php-fpm using service
-	if output, err := s.command.Run("service", []string{"php" + request.GetVersion() + "-fpm", "restart"}); err != nil {
+	if output, err := s.command.Run("service", []string{"php" + version + "-fpm", "restart"}); err != nil {
 		s.logger.Println("error restarting php-fpm, error:", err)
 		s.logger.Println("output:", string(output))
 		return nil, status.Errorf(codes.Unknown, string(output))
 	}
 
-	return &ServiceResponse{Message: "successfully changed the ini setting for " + setting + " to " + request.GetValue()}, nil
+	return &ServiceResponse{Message: "successfully changed the ini setting for " + setting + " to " + value}, nil
 }
