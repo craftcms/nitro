f52c8528979b1866ba0d922c8c0677362a27b940
Jason McCallister <jason@craftcms.com>
2020-07-24 12:33:51 -0400

    add multiple nameservers to resolve #186

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -118,7 +118,7 @@ write_files:
       xdebug.remote_autostart=1
       xdebug.idekey=PHPSTORM
 runcmd:
-  - sed -i 's|127.0.0.53|1.1.1.1|g' /etc/resolv.conf
+  - sed -i 's|nameserver 127.0.0.53|nameserver 127.0.0.53\nnameserver 1.1.1.1\nnameserver 1.0.0.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4|g' /etc/resolv.conf
   - add-apt-repository --no-update -y ppa:ondrej/php
   - echo "CRAFT_NITRO=1" >> /etc/environment
   - echo "DB_USER=nitro" >> /etc/environment
