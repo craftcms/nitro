2743f41dfac5d634620cb462923b0f112a2a3fa2
Jason McCallister <jason@craftcms.com>
2020-07-26 20:38:51 -0400

    cleanup
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- cmd/nitrod/main.go
+++ cmd/nitrod/main.go
@@ -12,19 +12,25 @@ import (
 )
 
 func main() {
+	// assign the port as a flag with a default
 	port := flag.String("port", "50051", "which port nitro API should listen on")
 	flag.Parse()
 
+	// create the network listener
 	lis, err := net.Listen("tcp", "0.0.0.0:"+*port)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// create the grpc server
 	s := grpc.NewServer()
+
+	// register our service
 	nitrod.RegisterNitroServiceServer(s, nitrod.New())
 
 	fmt.Println("running nitrod on port", *port)
 
+	// server the grpc service
 	if err := s.Serve(lis); err != nil {
 		log.Fatal("error when running the server", err)
 	}
