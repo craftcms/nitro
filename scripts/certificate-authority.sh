790d08546756f3672c89dedbedc45d3692f7c43c
Jason McCallister <jason@craftcms.com>
2020-07-31 08:19:04 -0400

    added mkdir

--- scripts/certificate-authority.sh
+++ scripts/certificate-authority.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 COMMON_NAME=nitro-dev
 
+# make the directories if they do not exist
+if [ ! -d "/etc/nitrod/certs" ]; then
+    mkdir -p /etc/nitrod/certs
+fi
+
 # files
 CA_KEY_PATH="/etc/nitrod/certs/ca.key"
 CA_TRUST_PATH="/etc/nitrod/certs/ca.crt"
-SERVER_PRIVATE_KEY_PATH="/etc/nitrod/certs/nitro.key"
-SERVER_CSR_PATH="/etc/nitrod/certs/nitro.csr"
+SERVER_PRIVATE_KEY_PATH="/etc/nitrod/certs/nitrod.key"
+SERVER_CSR_PATH="/etc/nitrod/certs/nitrod.csr"
 
 # generate the certificate authority if it does not exist
 if [ ! -f ${CA_KEY_PATH} ]; then
@@ -28,18 +33,18 @@ if [ ! -f ${CA_TRUST_PATH} ]; then
     -subj "/CN=${COMMON_NAME}"
 fi
 
-# generate a private key for the nitro server
+# generate a private key for the nitrod api
 if [ ! -f ${SERVER_PRIVATE_KEY_PATH} ]; then
-  echo "INFO: creating the server private key..."
+  echo "INFO: creating the nitrod private key..."
   openssl genrsa \
     -passout pass:1111 \
     -des3 \
     -out ${SERVER_PRIVATE_KEY_PATH} 4096
 fi
 
-# create the certificate signing request for the nitro server
+# create the certificate signing request for the nitrod api
 if [ ! -f ${SERVER_CSR_PATH} ]; then
-  echo "INFO: creating the server certificate signing request..."
+  echo "INFO: creating the server certificate signing request for nitrod..."
   openssl req \
     -passin pass:1111 \
     -new \
