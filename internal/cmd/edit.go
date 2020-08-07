99177d74e431b3c44ca3ff85f8bd9c67e4107963
Jason McCallister <jason@craftcms.com>
2020-08-07 13:38:46 -0400

    move editor pkg
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/edit.go
+++ internal/cmd/edit.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"github.com/craftcms/nitro/internal/editor"
+	"github.com/craftcms/nitro/editor"
 )
 
 var editCommand = &cobra.Command{
