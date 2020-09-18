5bb68408bff1edc1ed2c967bdd2843cdbe347187
Jason McCallister <jason@craftcms.com>
2020-09-18 00:59:17 -0400

    add nitrod to refresh.sh
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- refresh.sh
+++ refresh.sh
@@ -6,6 +6,20 @@ if [ -z "$version" ]; then
   exit 1
 fi
 
+# always download and install the nitrod api regardless of the version
+echo "installing nitrod and nitrod.service"
+curl -s https://api.github.com/repos/craftcms/nitro/releases/latest \
+  | grep "browser_download_url" \
+  | grep "nitrod_linux_x86_64" \
+  | cut -d : -f 2,3 | tr -d \" \
+  | wget --directory-prefix=/tmp -qi -
+cd /tmp && tar xfz /tmp/nitrod_linux_x86_64.tar.gz
+mv /tmp/nitrod /usr/sbin/
+mv /tmp/nitrod.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl start nitrod
+systemctl enable nitrod
+
 if [ "$version" == "1.0.0-RC1" ] || [ "$version" == "1.0.0-RC1.1" ]; then
   echo "running script for 1.0.0-RC1 and 1.0.0-RC1.1"
 
