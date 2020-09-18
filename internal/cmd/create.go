ed0e3ef9a3da8ef8c8a828e652f49a35e54081e9
Jason McCallister <jason@craftcms.com>
2020-09-17 23:15:14 -0400

    wip create command
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/cmd/create.go
+++ internal/cmd/create.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"archive/zip"
-	"bytes"
 	"errors"
 	"fmt"
 	"io"
@@ -10,12 +9,15 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/pixelandtonic/prompt"
 	"github.com/spf13/cobra"
 
 	"github.com/craftcms/nitro/internal/helpers"
 )
+
+const craftDownloadURL = "https://craftcms.com/latest-v3.zip"
 
 var createcommand = &cobra.Command{
 	Use:   "create",
@@ -31,67 +33,100 @@ var createcommand = &cobra.Command{
 			return err
 		}
 
+		// clean up the directory name
+		dir = strings.TrimSpace(strings.Replace(dir, " ", "-", -1))
+
 		if helpers.DirExists(dir) {
 			return errors.New(fmt.Sprintf("the directory %q already exists", dir))
 		}
 
+		projectPath, err := filepath.Abs(dir)
+		if err != nil {
+			return err
+		}
+
 		// download craft
-		url := "https://craftcms.com/latest-v3.zip"
-		resp, err := http.Get(url)
+		resp, err := http.Get(craftDownloadURL)
 		if err != nil {
 			return err
 		}
 		defer resp.Body.Close()
 
-		// Write the body to file
-		body, err := ioutil.ReadAll(resp.Body)
+		tmpDir := os.TempDir()
+
+		// create the temp file
+		tempFile, err := ioutil.TempFile(tmpDir, "nitro")
+		if err != nil {
+			return err
+		}
+		defer tempFile.Close()
+
+		fmt.Println("Downloading Craft CMS...")
+
+		// copy the downloaded contents into the temp file
+		_, err = io.Copy(tempFile, resp.Body)
 		if err != nil {
 			return err
 		}
 
-		// unzip the folder
-		zr, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
-		if err != nil {
+		// unzip the files
+		if err := unzip(tempFile, projectPath); err != nil {
 			return err
-		}
-
-		if err := os.Mkdir(dir, 0744); err != nil {
-			return err
-		}
-
-		for _, f := range zr.File {
-			p := filepath.Join(dir, f.Name)
-
-			// if this is a directory, lets make it
-			if f.FileInfo().IsDir() {
-				if err := os.MkdirAll(p, os.ModePerm); err != nil {
-					return err
-				}
-			}
-
-			// if its a file, make it
-			file, err := os.OpenFile(p, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-
-			rdr, err := f.Open()
-			if err != nil {
-				return err
-			}
-
-			if _, err := io.Copy(file, rdr); err != nil {
-				return err
-			}
-			if err := file.Close(); err != nil {
-				return err
-			}
-
-			if err := rdr.Close(); err != nil {
-				return err
-			}
 		}
 
 		return nil
 	},
+}
+
+func unzip(source *os.File, path string) error {
+	r, err := zip.OpenReader(source.Name())
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	// move files into place
+	for _, f := range r.File {
+		path := filepath.Join(path, f.Name)
+
+		// zipslip
+		if !strings.HasPrefix(path, filepath.Clean(path)+string(os.PathSeparator)) {
+			return fmt.Errorf("%s: illegal file path", path)
+		}
+
+		// create if a directory
+		if f.FileInfo().IsDir() {
+			if err := os.MkdirAll(path, os.ModePerm); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// create the files
+		if err = os.MkdirAll(filepath.Dir(path), os.ModePerm); err != nil {
+			return err
+		}
+
+		out, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+
+		readerCloser, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(out, readerCloser)
+
+		// cleanup
+		out.Close()
+		readerCloser.Close()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
