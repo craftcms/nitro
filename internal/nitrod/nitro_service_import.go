da1ba2810f3a629806462021f2e5d29e5a56d65a
Jason McCallister <jason@craftcms.com>
2020-09-23 11:51:44 -0400

    fix memory issue
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- internal/nitrod/nitro_service_import.go
+++ internal/nitrod/nitro_service_import.go
@@ -1,8 +1,6 @@
 package nitrod
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -12,19 +10,21 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-const maxSize = 1 << 20
-
 func (s *NitroService) ImportDatabase(stream NitroService_ImportDatabaseServer) error {
 	var container string
 	var database string
 
-	buffer := bytes.Buffer{}
-	backupSize := 0
+	// create a temp file
+	file, err := ioutil.TempFile(os.TempDir(), "nitro-database-import-")
+	if err != nil {
+		s.logger.Println(fmt.Errorf("error creating the temp file %v", err))
+		return status.Errorf(codes.FailedPrecondition, "Unable to create a temp file, err:", err.Error())
+	}
+	s.logger.Println("Created temporary file for database import", file.Name())
 
 	for {
 		req, err := stream.Recv()
 		if err == io.EOF {
-			s.logger.Println("size of backup is:", backupSize)
 			break
 		}
 		if err != nil {
@@ -36,38 +36,12 @@ func (s *NitroService) ImportDatabase(stream NitroService_ImportDatabaseServer) 
 
 		// get the backup
 		backup := req.GetData()
-		size := len(backup)
 
-		backupSize += size
-		if size > maxSize {
-			return status.Errorf(codes.InvalidArgument, "the backup size is too large")
-		}
-
-		// write the backup content into the buffer
-		_, err = buffer.Write(backup)
-		if err == bufio.ErrBufferFull {
-			return err
-		}
+		// write the backup content into the temp file
+		_, err = file.Write(backup)
 		if err != nil {
-			return status.Errorf(codes.Internal, "error writing to the buffer", err.Error())
+			return status.Errorf(codes.Internal, "unable to write the backup to the temp file")
 		}
-	}
-
-	s.logger.Println("out of the for loop")
-
-	// create a temp file
-	tempFile, err := ioutil.TempFile(os.TempDir(), "nitro-database-import-")
-	if err != nil {
-		s.logger.Println(fmt.Errorf("error creating the temp file %v", err))
-		return status.Errorf(codes.FailedPrecondition, "Unable to create a temp file, err:", err.Error())
-	}
-
-	s.logger.Println("Created temporary file for database import", tempFile.Name())
-
-	// save the data into the file
-	_, err = tempFile.Write(buffer.Bytes())
-	if err != nil {
-		return status.Errorf(codes.Internal, "unable to write the backup to the temp file")
 	}
 
 	s.logger.Printf("Creating database %s for container %s\n", database, container)
