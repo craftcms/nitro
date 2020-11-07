aa831ce6e60381d6a37d40ee43c35d25b056c248
Jason McCallister <jason@craftcms.com>
2020-11-07 18:22:49 -0500

    simplicity is key
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- pkg/client/init.go
+++ pkg/client/init.go
@@ -16,42 +16,17 @@ import (
 func (cli *Client) Init(ctx context.Context, name string, args []string) error {
 	fmt.Println("Running pre-checks on the development environment...")
 
-	// create filters
-	filter := filters.NewArgs()
-	filter.Add("name", name)
-
-	// check if the network needs to be created
-	if err := cli.checkNetwork(ctx, name, filter); err != nil {
-		return err
-	}
-
-	// check if the volume needs to be created
-	if err := cli.checkVolume(ctx, name, filter); err != nil {
-		return err
-	}
-
-	// pull the latest image from docker hub for the nitro-proxy
-	// TODO(jasonmccallister) replace with the nitro CLI version and non-local image (e.g. craftcms/nitro-proxy:version)
-	//_, err = cli.docker.ImagePull(ctx, "testing-caddy:latest", types.ImagePullOptions{})
-	//if err != nil {
-	//	return fmt.Errorf("unable to pull the nitro-proxy from docker hub, %w", err)
-	//}
-
 	// check if port 80, 443, and 5000 are available
 	ports := []string{"80", "443", "5000"}
 	if err := cli.checkPorts(ports); err != nil {
 		return err
 	}
 
-	// check if there is an existing container for the nitro-proxy
-	if err := cli.checkContainer(ctx, name, filter); err != nil {
-		return err
-	}
+	// create filters
+	filter := filters.NewArgs()
+	filter.Add("name", name)
 
-	return nil
-}
-
-func (cli *Client) checkNetwork(ctx context.Context, name string, filter filters.Args) error {
+	// check if the network needs to be created
 	networks, err := cli.docker.NetworkList(ctx, types.NetworkListOptions{Filters: filter})
 	if err != nil {
 		return fmt.Errorf("unable to list the docker networks, %w", err)
@@ -69,29 +44,24 @@ func (cli *Client) checkNetwork(ctx context.Context, name string, filter filters
 	// create the network needs to be created
 	if skipNetwork {
 		fmt.Println(" ==> Skipping network creation for", name)
+	} else {
+		fmt.Println(" ==> Creating network for", name)
 
-		return nil
+		resp, err := cli.docker.NetworkCreate(ctx, name, types.NetworkCreate{
+			Driver:     "bridge",
+			Attachable: true,
+			Labels: map[string]string{
+				"nitro": name,
+			},
+		})
+		if err != nil {
+			return fmt.Errorf("unable to create the network, %w", err)
+		}
+
+		fmt.Println(" ==> network created with id", resp.ID)
 	}
 
-	fmt.Println(" ==> Creating network for", name)
-
-	resp, err := cli.docker.NetworkCreate(ctx, name, types.NetworkCreate{
-		Driver:     "bridge",
-		Attachable: true,
-		Labels: map[string]string{
-			"nitro": name,
-		},
-	})
-	if err != nil {
-		return fmt.Errorf("unable to create the network, %w", err)
-	}
-
-	fmt.Println(" ==> network created with id", resp.ID)
-
-	return nil
-}
-
-func (cli *Client) checkVolume(ctx context.Context, name string, filter filters.Args) error {
+	// check if the volume needs to be created
 	volumes, err := cli.docker.VolumeList(ctx, filter)
 	if err != nil {
 		return fmt.Errorf("unable to list the docker volumes, %w", err)
@@ -109,25 +79,36 @@ func (cli *Client) checkVolume(ctx context.Context, name string, filter filters.
 	// check if the volume needs to be created
 	if skipVolume {
 		fmt.Println(" ==> Skipping volume creation for", name)
+	} else {
 
-		return nil
+		fmt.Println(" ==> Creating volume for", name)
+
+		// create a volume with the same name of the machine
+		resp, err := cli.docker.VolumeCreate(ctx, volumetypes.VolumesCreateBody{
+			Driver: "local",
+			Name:   name,
+			Labels: map[string]string{
+				"nitro": name,
+			},
+		})
+		if err != nil {
+			return fmt.Errorf("unable to create the network, %w", err)
+		}
+
+		fmt.Println(" ==> volume created with name", resp.Name)
 	}
 
-	fmt.Println(" ==> Creating volume for", name)
+	// pull the latest image from docker hub for the nitro-proxy
+	// TODO(jasonmccallister) replace with the nitro CLI version and non-local image (e.g. craftcms/nitro-proxy:version)
+	//_, err = cli.docker.ImagePull(ctx, "testing-caddy:latest", types.ImagePullOptions{})
+	//if err != nil {
+	//	return fmt.Errorf("unable to pull the nitro-proxy from docker hub, %w", err)
+	//}
 
-	// create a volume with the same name of the machine
-	resp, err := cli.docker.VolumeCreate(ctx, volumetypes.VolumesCreateBody{
-		Driver: "local",
-		Name:   name,
-		Labels: map[string]string{
-			"nitro": name,
-		},
-	})
-	if err != nil {
-		return fmt.Errorf("unable to create the network, %w", err)
+	// check if there is an existing container for the nitro-proxy
+	if err := cli.checkContainer(ctx, name, filter); err != nil {
+		return err
 	}
-
-	fmt.Println(" ==> volume created with name", resp.Name)
 
 	return nil
 }
