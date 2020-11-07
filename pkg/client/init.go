fa9c3276bd7a9469cd294492111c0b028e2095c3
Jason McCallister <jason@craftcms.com>
2020-11-07 18:13:59 -0500

    clean up
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- pkg/client/init.go
+++ pkg/client/init.go
@@ -20,12 +20,12 @@ func (cli *Client) Init(ctx context.Context, name string, args []string) error {
 	filter := filters.NewArgs()
 	filter.Add("name", name)
 
-	// get all the networks filtered by name
+	// check if the network needs to be created
 	if err := cli.checkNetwork(ctx, name, filter); err != nil {
 		return err
 	}
 
-	// get all the volumes filtered by name
+	// check if the volume needs to be created
 	if err := cli.checkVolume(ctx, name, filter); err != nil {
 		return err
 	}
@@ -44,6 +44,95 @@ func (cli *Client) Init(ctx context.Context, name string, args []string) error {
 	}
 
 	// check if there is an existing container for the nitro-proxy
+	if err := cli.checkContainer(ctx, name, filter); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cli *Client) checkNetwork(ctx context.Context, name string, filter filters.Args) error {
+	networks, err := cli.docker.NetworkList(ctx, types.NetworkListOptions{Filters: filter})
+	if err != nil {
+		return fmt.Errorf("unable to list the docker networks, %w", err)
+	}
+
+	// since the filter is fuzzy, do an exact match (e.g. filtering for
+	// `nitro-dev` will also return `nitro-dev-host`
+	var skipNetwork bool
+	for _, n := range networks {
+		if n.Name == name {
+			skipNetwork = true
+		}
+	}
+
+	// create the network needs to be created
+	if skipNetwork {
+		fmt.Println(" ==> Skipping network creation for", name)
+
+		return nil
+	}
+
+	fmt.Println(" ==> Creating network for", name)
+
+	resp, err := cli.docker.NetworkCreate(ctx, name, types.NetworkCreate{
+		Driver:     "bridge",
+		Attachable: true,
+		Labels: map[string]string{
+			"nitro": name,
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("unable to create the network, %w", err)
+	}
+
+	fmt.Println(" ==> network created with id", resp.ID)
+
+	return nil
+}
+
+func (cli *Client) checkVolume(ctx context.Context, name string, filter filters.Args) error {
+	volumes, err := cli.docker.VolumeList(ctx, filter)
+	if err != nil {
+		return fmt.Errorf("unable to list the docker volumes, %w", err)
+	}
+
+	// since the filter is fuzzy, do an exact match (e.g. filtering for
+	// `nitro-dev` will also return `nitro-dev-host`
+	var skipVolume bool
+	for _, volume := range volumes.Volumes {
+		if volume.Name == name {
+			skipVolume = true
+		}
+	}
+
+	// check if the volume needs to be created
+	if skipVolume {
+		fmt.Println(" ==> Skipping volume creation for", name)
+
+		return nil
+	}
+
+	fmt.Println(" ==> Creating volume for", name)
+
+	// create a volume with the same name of the machine
+	resp, err := cli.docker.VolumeCreate(ctx, volumetypes.VolumesCreateBody{
+		Driver: "local",
+		Name:   name,
+		Labels: map[string]string{
+			"nitro": name,
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("unable to create the network, %w", err)
+	}
+
+	fmt.Println(" ==> volume created with name", resp.Name)
+
+	return nil
+}
+
+func (cli *Client) checkContainer(ctx context.Context, name string, filter filters.Args) error {
 	containers, err := cli.docker.ContainerList(ctx, types.ContainerListOptions{Filters: filter})
 	if err != nil {
 		return fmt.Errorf("unable to list the containers\n%w", err)
@@ -109,90 +198,16 @@ func (cli *Client) Init(ctx context.Context, name string, args []string) error {
 	return nil
 }
 
-func (cli *Client) checkNetwork(ctx context.Context, name string, filter filters.Args) error {
-	networks, err := cli.docker.NetworkList(ctx, types.NetworkListOptions{Filters: filter})
-	if err != nil {
-		return fmt.Errorf("unable to list the docker networks, %w", err)
-	}
-
-	// since the filter is fuzzy, do an exact match (e.g. filtering for
-	// `nitro-dev` will also return `nitro-dev-host`
-	var skipNetwork bool
-	for _, n := range networks {
-		if n.Name == name {
-			skipNetwork = true
-		}
-	}
-
-	// create the network needs to be created
-	if skipNetwork {
-		fmt.Println(" ==> Skipping network creation for", name)
-	} else {
-		fmt.Println(" ==> Creating network for", name)
-
-		resp, err := cli.docker.NetworkCreate(ctx, name, types.NetworkCreate{
-			Driver:     "bridge",
-			Attachable: true,
-			Labels: map[string]string{
-				"nitro": name,
-			},
-		})
-		if err != nil {
-			return fmt.Errorf("unable to create the network, %w", err)
-		}
-
-		fmt.Println(" ==> network created with id", resp.ID)
-	}
-
-	return nil
-}
-
-func (cli *Client) checkVolume(ctx context.Context, name string, filter filters.Args) error {
-	volumes, err := cli.docker.VolumeList(ctx, filter)
-	if err != nil {
-		return fmt.Errorf("unable to list the docker volumes, %w", err)
-	}
-
-	// since the filter is fuzzy, do an exact match (e.g. filtering for
-	// `nitro-dev` will also return `nitro-dev-host`
-	var skipVolume bool
-	for _, volume := range volumes.Volumes {
-		if volume.Name == name {
-			skipVolume = true
-		}
-	}
-
-	// check if the volume needs to be created
-	if skipVolume {
-		fmt.Println(" ==> Skipping volume creation for", name)
-	} else {
-		fmt.Println(" ==> Creating volume for", name)
-
-		// create a volume with the same name of the machine
-		resp, err := cli.docker.VolumeCreate(ctx, volumetypes.VolumesCreateBody{
-			Driver: "local",
-			Name:   name,
-			Labels: map[string]string{
-				"nitro": name,
-			},
-		})
-		if err != nil {
-			return fmt.Errorf("unable to create the network, %w", err)
-		}
-
-		fmt.Println(" ==> volume created with name", resp.Name)
-	}
-
-	return nil
-}
-
 func (cli *Client) checkPorts(ports []string) error {
 	for _, port := range ports {
 		lis, err := net.Listen("tcp", ":"+port)
 		if err != nil {
-			return fmt.Errorf("nitro uses ports 80, 443, and 5000. It appears the port %q, is already in use", port)
+			return fmt.Errorf("nitro uses ports 80, 443, and 5000. It appears port %q, is already in use", port)
 		}
-		lis.Close()
+
+		if err := lis.Close(); err != nil {
+			return fmt.Errorf("unable to close the listener after checking the ports, %w", err)
+		}
 	}
 
 	return nil
