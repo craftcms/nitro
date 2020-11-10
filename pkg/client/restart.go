8a01aaf63b3b227260df30cd3bde54f5b4474326
Jason McCallister <jason@craftcms.com>
2020-11-09 22:15:59 -0500

    time a timeout
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- pkg/client/restart.go
+++ pkg/client/restart.go
@@ -11,7 +11,7 @@ import (
 
 // Restart is used to restart all containers related to a specific environment
 func (cli *Client) Restart(ctx context.Context, name string, args []string) error {
-	fmt.Println("Restarting all containers for environment", name)
+	fmt.Println("Restarting containers for environment", name)
 
 	// get all the containers using a filter, we only want to restart containers which
 	// have the label com.craftcms.nitro.environment=name
@@ -22,12 +22,15 @@ func (cli *Client) Restart(ctx context.Context, name string, args []string) erro
 		return fmt.Errorf("unable to get a list of the containers, %w", err)
 	}
 
+	// set a timeout, consider making this a flag
+	timeout := time.Duration(5000) * time.Millisecond
+
 	// restart each container for the environment
 	for _, container := range containers {
-		// TODO maket this more dynamic
+		// TODO make this more dynamic
 		fmt.Println("  ==> restarting container for", container.Labels["com.craftcms.nitro.host"])
 
-		if err := cli.docker.ContainerRestart(ctx, container.ID, time.Duration(time.Second*5)); err != nil {
+		if err := cli.docker.ContainerRestart(ctx, container.ID, &timeout); err != nil {
 			return fmt.Errorf("unable to restart container %s: %w", container.Names[0], err)
 		}
 	}
