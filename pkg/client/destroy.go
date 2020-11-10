7433070ea39a66c6a363ee61ffd8051f3a29e675
Jason McCallister <jason@craftcms.com>
2020-11-10 07:56:11 -0500

    boilerplate for destroy
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- pkg/client/destroy.go
+++ pkg/client/destroy.go
@@ -3,14 +3,43 @@ package client
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 // Destroy is used to completed remove all containers, networks, and volumes for an environment.
 // is it a destructive action and will prompt a user for verification and perform a database
 // backup before removing the resources.
 func (cli *Client) Destroy(ctx context.Context, name string, args []string) error {
+	// TODO(jasonmccallister) get all related containers
+	// TODO(jasonmccallister) get all related volumes
+	// TODO(jasonmccallister) get all related networks
 
 	// if there are no containers, were done
+	// get all the containers
+	fmt.Println("Backing up databases")
+	fmt.Println("  ==> stopping fake container name")
+	time.Sleep(time.Second * 2)
+
+
+	// stop all of the container
+	fmt.Println("Stopping containers")
+	fmt.Println("  ==> stopping fake container name")
+	time.Sleep(time.Second * 2)
+
+	// remove all containers
+	fmt.Println("Removing containers")
+	fmt.Println("  ==> removing fake container name")
+	time.Sleep(time.Second * 2)
+
+	// get all the volumes
+	fmt.Println("Removing volumes")
+	fmt.Println("  ==> removing volume name")
+	time.Sleep(time.Second * 2)
+
+	// get all the networks
+	fmt.Println("Removing network")
+	fmt.Println("  ==> removing network name")
+	time.Sleep(time.Second * 2)
 
 	return fmt.Errorf("not yet implemented")
 }
--- pkg/cmd/destroy/main.go
+++ pkg/cmd/destroy/main.go
@@ -2,9 +2,11 @@ package destroy
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
 
 	"github.com/craftcms/nitro/pkg/client"
-	"github.com/spf13/cobra"
 )
 
 // DestroyCommand is the command for creating new development environments
@@ -16,6 +18,29 @@ var DestroyCommand = &cobra.Command{
 
 func destroy(cmd *cobra.Command, args []string) error {
 	env := cmd.Flag("environment").Value.String()
+
+	fmt.Println("Are you sure? This will remove all containers, volumes, and networks.")
+
+	// prompt the user for confirmation
+	var response string
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		return fmt.Errorf("unable to provide a prompt, %w", err)
+	}
+
+	var confirm bool
+	resp := strings.TrimSpace(response)
+	for _, answer := range []string{"y", "Y", "yes", "Yes", "YES"} {
+		if resp == answer {
+			confirm = true
+		}
+	}
+
+	if !confirm {
+		fmt.Println("Skipping destroy, all resources related to", env, "will remain")
+
+		return nil
+	}
 
 	// create the new client
 	nitro, err := client.NewClient()
