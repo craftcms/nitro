be0928cda1de39a5ac3373e5a8c2b057fec18fd6
Jason McCallister <jason@craftcms.com>
2020-11-09 21:52:58 -0500

    wip composer install

--- pkg/client/composer.go
+++ pkg/client/composer.go
@@ -3,22 +3,90 @@ package client
 import (
 	"context"
 	"fmt"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/pkg/stdcopy"
 )
 
-func Composer(ctx context.Context, absDir, version, action string) error {
-	// TODO check if there is a composer.json
+func (cli *Client) Composer(ctx context.Context, dir, version, action string) error {
+	image := fmt.Sprintf("docker.io/library/%s:%s", "composer", version)
 
-	// TODO create the temp container
+	// pull container
+	fmt.Println("Pulling composer image")
+	_, err := cli.docker.ImagePull(ctx, image, types.ImagePullOptions{})
+	if err != nil {
+		return fmt.Errorf("unable to pull the docker image, %w", err)
+	}
 
 	// get the version from the flag, default to 1
+	var cmd []string
 	switch action {
-	case "update":
-		fmt.Print("Running composer update with version", version)
-		// docker run --rm --interactive --tty --volume absDir:/app composer:version update --ignore-platform-reqs
+	case "install":
+		cmd = []string{"composer", "install", "--ignore-platform-reqs", "--prefer-dist"}
 	default:
-		fmt.Print("Running composer install with version", version)
-		// docker run --rm --interactive --tty --volume absDir:/app composer:version install --ignore-platform-reqs
+		cmd = []string{"composer", "update", "--ignore-platform-reqs", "--prefer-dist"}
 	}
+
+	fmt.Println("  ==> creating temporary container")
+
+	// create the temp container
+	resp, err := cli.docker.ContainerCreate(ctx,
+		&container.Config{
+			Image: image,
+			Cmd:   cmd,
+			Tty:   false,
+		},
+		&container.HostConfig{
+			Mounts: []mount.Mount{{
+				Type:   "bind",
+				Source: dir,
+				Target: "/app",
+			},
+			}},
+		nil,
+		"")
+	if err != nil {
+		return fmt.Errorf("unable to create the composer container\n%w", err)
+	}
+
+	fmt.Println("  ==> running composer, please stand by")
+
+	stream, err := cli.docker.ContainerAttach(ctx, resp.ID, types.ContainerAttachOptions{
+		Stream: true,
+		Stdout: true,
+		Stderr: true,
+		Logs:   true,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to attach to container, %w", err)
+	}
+	defer stream.Close()
+
+	// run the container
+	if err := cli.docker.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return fmt.Errorf("unable to start the container, %w", err)
+	}
+
+	if _, err := stdcopy.StdCopy(os.Stdout, os.Stderr, stream.Reader); err != nil {
+		return fmt.Errorf("unable to copy the output of the container logs, %w", err)
+	}
+
+	// if _, err := cli.docker.ContainerWait(ctx, resp.ID); err != nil {
+	// 	return fmt.Errorf("error while waiting for the container, %w", err)
+	// }
+
+	fmt.Println("Composer", action, "ran successfully")
+
+	fmt.Println("  ==> removing temporary container")
+
+	// if err := cli.docker.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{Force: true}); err != nil {
+	// 	return fmt.Errorf("unable to remove the temporary container %q, %w", resp.ID, err)
+	// }
+
+	fmt.Println("Cleanup completed")
 
 	return nil
 }
