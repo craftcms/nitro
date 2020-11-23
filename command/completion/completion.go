25a1247fac321ae12ea73bf3ed91af110aa49d63
Jason McCallister <jason@craftcms.com>
2020-11-23 08:13:58 -0500

    add completion command

---
+++ command/completion/completion.go
@@ -0,0 +1,34 @@
+package example
+
+import (
+	"fmt"
+
+	"github.com/craftcms/nitro/terminal"
+	"github.com/docker/docker/client"
+	"github.com/spf13/cobra"
+)
+
+var (
+	// ErrExample is used when we want to share an error
+	ErrExample = fmt.Errorf("not implemented")
+)
+
+const exampleText = `  # example command
+  nitro example`
+
+// New is used for scaffolding new commands
+func New(docker client.CommonAPIClient, output terminal.Outputer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "example",
+		Short:   "Example command",
+		Example: exampleText,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return ErrExample
+		},
+	}
+
+	// set flags for the command
+	cmd.Flags().StringP("example", "e", "example", "an example flag")
+
+	return cmd
+}
