392cd009968b17870909098e60718581f2554355
Jason McCallister <jason@craftcms.com>
2020-11-25 19:39:09 -0500

    add client for gRPC API

---
+++ pkg/client/client.go
@@ -0,0 +1,20 @@
+package client
+
+import (
+	"fmt"
+
+	"github.com/craftcms/nitro/protob"
+	"google.golang.org/grpc"
+)
+
+// NewClient is used for generating a new client
+// to interact with the gRPC API running in the
+// proxy container
+func NewClient(ip, port string) (protob.NitroClient, error) {
+	cc, err := grpc.Dial(ip+":"+port, grpc.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("unable to create a gRPC client, %w", err)
+	}
+
+	return protob.NewNitroClient(cc), nil
+}
