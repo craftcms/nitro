562914c8e7c40a5f320f8d903de7b7ef0c2c1ced
Jason McCallister <jason@craftcms.com>
2020-12-03 20:38:33 -0500

    wip

--- command/apply/internal/match/match.go
+++ command/apply/internal/match/match.go
@@ -1,6 +1,16 @@
 package match
 
-import "github.com/docker/docker/api/types"
+import (
+	"github.com/docker/docker/api/types"
+)
+
+// TODO(jasonmccallister) determine if we need this
+type containerInstance struct {
+	Hostname  string
+	Image     string
+	Container types.Container
+	Envs      []string
+}
 
 // Mounts takes a containers existing mounts against the sites expected mounts
 // and returns true if the mounts do not match.
