9be5ea13a93fdc533fc342ea4b707c1f33a6d2cd
Jason McCallister <jason@craftcms.com>
2020-12-10 08:48:40 -0500

    add tests to site match

--- command/apply/internal/match/match.go
+++ command/apply/internal/match/match.go
@@ -25,11 +25,15 @@ func Mounts(existing []types.MountPoint, expected map[string]string) bool {
 	return true
 }
 
+// Site takes the home directory, site, php config, and a container to determine if they
+// match whats expected.
 func Site(home string, site config.Site, php config.PHP, container types.Container) bool {
 	// check if the image does not match
 	if fmt.Sprintf("docker.io/craftcms/nginx:%s-dev", site.PHP) != container.Image {
 		return false
 	}
+
+	// check the environment variables
 
 	// check the mounts
 
--- command/apply/internal/match/match_test.go
+++ command/apply/internal/match/match_test.go
@@ -3,6 +3,7 @@ package match
 import (
 	"testing"
 
+	"github.com/craftcms/nitro/config"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/mount"
 )
@@ -59,6 +60,54 @@ func TestMounts(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Mounts(tt.args.existing, tt.args.expected); got != tt.want {
 				t.Errorf("Mounts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSite(t *testing.T) {
+	type args struct {
+		home      string
+		site      config.Site
+		php       config.PHP
+		container types.Container
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "path mismatches return false",
+			args: args{
+				home: "testdata/example-site",
+				site: config.Site{
+					Path: "testdata/new-site",
+					PHP:  "7.4",
+				},
+				container: types.Container{
+					Image: "docker.io/craftcms/nginx:7.4-dev",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "mismatched images return false",
+			args: args{
+				site: config.Site{
+					PHP: "7.4",
+				},
+				container: types.Container{
+					Image: "docker.io/craftcms/nginx:7.3-dev",
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Site(tt.args.home, tt.args.site, tt.args.php, tt.args.container); got != tt.want {
+				t.Errorf("Site() = %v, want %v", got, tt.want)
 			}
 		})
 	}
