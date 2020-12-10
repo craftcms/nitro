f1dd50f4f9a047e0c6c8889a1773725b7a8e37f6
Jason McCallister <jason@craftcms.com>
2020-12-10 16:48:03 -0500

    docs and cleanup

--- hostedit/hostedit.go
+++ hostedit/hostedit.go
@@ -1,18 +1,18 @@
 package hostedit
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"strings"
 )
 
 const (
-	StartText = "# <nitro>"
-	EndText   = "# </nitro>"
+	startText = "# <nitro>"
+	endText   = "# </nitro>"
 )
 
+// Update takes a file, reads the content and updates or appends
+// the addr and hosts for the sites.
 func Update(file, addr string, hosts ...string) (string, error) {
 	f, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -27,13 +27,13 @@ func Update(file, addr string, hosts ...string) (string, error) {
 	var index int
 	for l, t := range lines {
 		// look for the beginning text
-		if strings.Contains(t, StartText) {
+		if strings.Contains(t, startText) {
 			// the next line is the empty line
 			index = l + 1
 		}
 
 		// look for the end text
-		if strings.Contains(t, EndText) {
+		if strings.Contains(t, endText) {
 			// we want the previous line
 			index = l - 1
 		}
@@ -42,34 +42,13 @@ func Update(file, addr string, hosts ...string) (string, error) {
 	switch index {
 	// if there is not a comment section, we need to create one
 	case 0:
-		lines = append(lines, StartText)
+		lines = append(lines, startText)
 		lines = append(lines, fmt.Sprintf("%s\t%s", addr, strings.Join(hosts, " ")))
-		lines = append(lines, EndText+"\n")
+		lines = append(lines, endText+"\n")
 	default:
 		// replace the line between the start and end text with the contents of the address and hosts
 		lines[index] = fmt.Sprintf("%s\t%s", addr, strings.Join(hosts, " "))
 	}
 
 	return strings.Join(lines, "\n"), nil
-}
-
-func find(r io.Reader) (int, int) {
-	scanner := bufio.NewScanner(r)
-
-	// get the
-	var start, end, index int
-	for scanner.Scan() {
-		index = index + 1
-		if strings.Contains(scanner.Text(), StartText) {
-			start = index
-		}
-
-		if strings.Contains(scanner.Text(), EndText) {
-			end = index
-			// once we have the end, we can stop here
-			break
-		}
-	}
-
-	return start, end
 }
