a1f9f2de31d15533843eda1ffff9301cd13ec26a
Jason McCallister <jason@craftcms.com>
2020-12-10 16:05:19 -0500

    complete wip Update

--- hostedit/hostedit.go
+++ hostedit/hostedit.go
@@ -2,32 +2,55 @@ package hostedit
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"strings"
 )
 
-// load the file
-
 const (
 	StartText = "# <nitro>"
 	EndText   = "# </nitro>"
 )
 
-func Update(file string) (string, error) {
+func Update(file, addr string, hosts ...string) (string, error) {
 	f, err := ioutil.ReadFile(file)
 	if err != nil {
 		return "", err
 	}
 
-	start, end := find(bytes.NewBuffer(f))
+	// split the file into multiple lines
+	lines := strings.Split(string(f), "\n")
 
-	fmt.Println(start)
-	fmt.Println(end)
+	// the index represents where the content (addr and hosts) should be placed
+	// which is in between the start and end text comment
+	var index int
+	for l, t := range lines {
+		// look for the beginning text
+		if strings.Contains(t, StartText) {
+			// the next line is the empty line
+			index = l + 1
+		}
 
-	return "", nil
+		// look for the end text
+		if strings.Contains(t, EndText) {
+			// we want the previous line
+			index = l - 1
+		}
+	}
+
+	switch index {
+	// if there is not a comment section, we need to create one
+	case 0:
+		lines = append(lines, StartText)
+		lines = append(lines, fmt.Sprintf("%s\t%s", addr, strings.Join(hosts, " ")))
+		lines = append(lines, EndText+"\n")
+	default:
+		// replace the line between the start and end text with the contents of the address and hosts
+		lines[index] = fmt.Sprintf("%s\t%s", addr, strings.Join(hosts, " "))
+	}
+
+	return strings.Join(lines, "\n"), nil
 }
 
 func find(r io.Reader) (int, int) {
--- hostedit/hostedit_test.go
+++ hostedit/hostedit_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestGetSection(t *testing.T) {
+func TestFind(t *testing.T) {
 	type args struct {
 		f string
 	}
@@ -41,6 +41,88 @@ func TestGetSection(t *testing.T) {
 			}
 			if got1 != tt.wantEnd {
 				t.Errorf("GetSection() got1 = %v, want %v", got1, tt.wantEnd)
+			}
+		})
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	type args struct {
+		file  string
+		addr  string
+		hosts []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "can append to the file when no section is found",
+			args: args{file: "testdata/no-section.txt", addr: "127.0.0.1", hosts: []string{"one", "two", "three"}},
+			want: `##
+# Host Database
+#
+# localhost is used to configure the loopback interface
+# when the system is booting.  Do not change this entry.
+##
+127.0.0.1        localhost
+255.255.255.255  broadcasthost
+::1              localhost
+
+127.0.0.1        kubernetes.docker.internal
+# Added by Docker Desktop
+# To allow the same kube context to work on the host and the container:
+127.0.0.1        kubernetes.docker.internal
+# End of section
+
+# <nitro>
+127.0.0.1	one two three
+# </nitro>
+`,
+		},
+		{
+			name: "can update the right section",
+			args: args{file: "testdata/has-section.txt", addr: "127.0.0.1", hosts: []string{"one", "two", "three"}},
+			want: `##
+# Host Database
+#
+# localhost is used to configure the loopback interface
+# when the system is booting.  Do not change this entry.
+##
+127.0.0.1        localhost
+255.255.255.255  broadcasthost
+::1              localhost
+
+# <nitro>
+127.0.0.1	one two three
+# </nitro>
+
+127.0.0.1        kubernetes.docker.internal
+# Added by Docker Desktop
+# To allow the same kube context to work on the host and the container:
+127.0.0.1        kubernetes.docker.internal
+# End of section
+`,
+		},
+		{
+			name:    "no file returns an error",
+			args:    args{file: "testdata/empty"},
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Update(tt.args.file, tt.args.addr, tt.args.hosts...)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Update() = %v, want %v", got, tt.want)
 			}
 		})
 	}
