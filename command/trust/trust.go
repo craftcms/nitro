1ffb7a802b190bba00672aa68e291610e8a84f86
Jason McCallister <jason@craftcms.com>
2020-12-11 17:42:51 -0500

    remove linux and windows specific builds

--- command/trust/trust.go
+++ command/trust/trust.go
@@ -9,6 +9,8 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/craftcms/nitro/labels"
 	"github.com/craftcms/nitro/pkg/sudo"
@@ -98,13 +100,45 @@ func New(docker client.CommonAPIClient, output terminal.Outputer) *cobra.Command
 
 			output.Info("Installing certificate (you might be prompted for your password)")
 
-			// add the certificate to the keychain
-			if err := sudo.Run("security", "security", "add-trusted-cert", "-d", "-r", "trustRoot", "-k", "/Library/Keychains/System.keychain", f.Name()); err != nil {
-				output.Info("Unable to automatically add certificate\n")
-				output.Info("To install the certificate, run the following command:")
-				output.Info(fmt.Sprintf("  sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain %s", f.Name()))
+			switch runtime.GOOS {
+			case "linux":
+				// using the reference from: https://askubuntu.com/questions/645818/how-to-install-certificates-for-command-line
+				if err := sudo.Run("mv", "mv", f.Name(), fmt.Sprintf("/usr/local/share/ca-certificates/%s.crt", env)); err != nil {
+					output.Info("Unable to automatically add certificate\n")
+					output.Info("To install the certificate, run the following command:")
+					output.Info(fmt.Sprintf("  sudo mv %s /usr/local/share/ca-certificates/%s.crt", f.Name(), env))
+					output.Info("  sudo update-ca-certificates")
 
-				return nil
+					return nil
+				}
+
+				// update the ca certs
+				if err := sudo.Run("update-ca-certificates", "update-ca-certificates"); err != nil {
+					output.Info("Unable to automatically add certificate\n")
+					output.Info("To install the certificate, run the following command:")
+					output.Info("  sudo update-ca-certificates")
+
+					return nil
+				}
+			case "windows":
+				// automate the certificate installation from this article: https://superuser.com/a/1506481/215387
+				// we cannot assume PowerShell is enabled, so we use certutil.exe
+				if err := exec.Command("certutil.exe", "-addstore", "root", f.Name()).Run(); err != nil {
+					output.Info("Unable to automatically add certificate\n")
+					output.Info("To install the certificate, run the following command:")
+					output.Info(fmt.Sprintf("  certutil.exe -addstore root %s", f.Name()))
+
+					return nil
+				}
+			default:
+				// add the certificate to the keychain
+				if err := sudo.Run("security", "security", "add-trusted-cert", "-d", "-r", "trustRoot", "-k", "/Library/Keychains/System.keychain", f.Name()); err != nil {
+					output.Info("Unable to automatically add certificate\n")
+					output.Info("To install the certificate, run the following command:")
+					output.Info(fmt.Sprintf("  sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain %s", f.Name()))
+
+					return nil
+				}
 			}
 
 			// clean up
