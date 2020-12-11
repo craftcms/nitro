c1385caf9725bf5299eebd13a234d1a653c27a03
Jason McCallister <jason@craftcms.com>
2020-12-11 17:38:47 -0500

    remove dupe vars

--- command/trust/trust_win.go
+++ command/trust/trust_win.go
@@ -19,14 +19,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	// ErrNoContainers is returned when no containers are running for an environment
-	ErrNoContainers = fmt.Errorf("there are no running containers")
-)
-
-const exampleText = `  # get the certificates for an environment
-  nitro trust`
-
 // New returns `trust` to retreive the certificates from the nitro proxy and install on the
 // host machine. The CA is used to sign certificates for websites and adding the certificate
 // to the system allows TLS connections to be considered valid and trusted from the container.
