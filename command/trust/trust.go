44a4d1214cda9e61c0dad27dbd1e05f2283bdf65
Jason McCallister <jason@craftcms.com>
2020-12-11 13:18:37 -0500

    remove os specific cmd

--- command/trust/trust.go
+++ command/trust/trust.go
@@ -9,8 +9,6 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	"os/exec"
-	"runtime"
 
 	"github.com/craftcms/nitro/labels"
 	"github.com/craftcms/nitro/pkg/sudo"
@@ -99,48 +97,14 @@ func New(docker client.CommonAPIClient, output terminal.Outputer) *cobra.Command
 			output.Done()
 
 			output.Info("Installing certificate (you might be prompted for your password)")
-			switch runtime.GOOS {
-			case "linux":
-				// using the reference from: https://askubuntu.com/questions/645818/how-to-install-certificates-for-command-line
-				if err := sudo.Run("mv", "mv", f.Name(), fmt.Sprintf("/usr/local/share/ca-certificates/%s.crt", env)); err != nil {
-					output.Info("Unable to automatically add certificate\n")
-					output.Info("To install the certificate, run the following command:")
-					output.Info(fmt.Sprintf("  sudo mv %s /usr/local/share/ca-certificates/%s.crt", f.Name(), env))
-					output.Info("  sudo update-ca-certificates")
 
-					return nil
-				}
+			// add the certificate to the keychain
+			if err := sudo.Run("security", "security", "add-trusted-cert", "-d", "-r", "trustRoot", "-k", "/Library/Keychains/System.keychain", f.Name()); err != nil {
+				output.Info("Unable to automatically add certificate\n")
+				output.Info("To install the certificate, run the following command:")
+				output.Info(fmt.Sprintf("  sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain %s", f.Name()))
 
-				// update the ca certs
-				if err := sudo.Run("update-ca-certificates", "update-ca-certificates"); err != nil {
-					output.Info("Unable to automatically add certificate\n")
-					output.Info("To install the certificate, run the following command:")
-					output.Info("  sudo update-ca-certificates")
-
-					return nil
-				}
-				// TODO(jasonmccallister) trust the certificate for firefox and edge on linux (https://javorszky.co.uk/2019/11/06/get-firefox-to-trust-your-self-signed-certificates/)
-			// windows
-			case "windows":
-				// automate the certificate installation from this article: https://superuser.com/a/1506481/215387
-				// we cannot assume PowerShell is enabled, so we use certutil.exe
-				if err := exec.Command("certutil.exe", "-addstore", "root", f.Name()).Run(); err != nil {
-					output.Info("Unable to automatically add certificate\n")
-					output.Info("To install the certificate, run the following command:")
-					output.Info(fmt.Sprintf("  certutil.exe -addstore root %s", f.Name()))
-
-					return nil
-				}
-			// macos
-			default:
-				// add the certificate to the keychain
-				if err := sudo.Run("security", "security", "add-trusted-cert", "-d", "-r", "trustRoot", "-k", "/Library/Keychains/System.keychain", f.Name()); err != nil {
-					output.Info("Unable to automatically add certificate\n")
-					output.Info("To install the certificate, run the following command:")
-					output.Info(fmt.Sprintf("  sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain %s", f.Name()))
-
-					return nil
-				}
+				return nil
 			}
 
 			// clean up
