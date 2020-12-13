035ed4bad88780655174c2acca1f7505f993cf12
Jason McCallister <jason@craftcms.com>
2020-12-12 20:49:18 -0500

    remove old type

--- command/apply/internal/match/match.go
+++ command/apply/internal/match/match.go
@@ -8,14 +8,6 @@ import (
 	"github.com/docker/docker/api/types"
 )
 
-// TODO(jasonmccallister) determine if we need this
-type containerInstance struct {
-	Hostname  string
-	Image     string
-	Container types.Container
-	Envs      []string
-}
-
 // Mounts takes a containers existing mounts against the sites expected mounts
 // and returns true if the mounts do not match.
 func Mounts(existing []types.MountPoint, expected map[string]string) bool {
