089e933f653ae5c945d676f3415e24296b1fff27
Jason McCallister <jason@craftcms.com>
2020-12-16 22:25:53 -0500

    enable xdebug and save the file

--- command/xon/xon.go
+++ command/xon/xon.go
@@ -14,6 +14,8 @@ import (
 const exampleText = `  # example command
   nitro xon`
 
+// NewCommand returns the command that is used to enable xdebug for a sepcific site. It will first check
+// if the current working directory or prompt the user for a site.
 func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outputer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "xon",
@@ -22,43 +24,54 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 		RunE: func(cmd *cobra.Command, args []string) error {
 			env := cmd.Flag("environment").Value.String()
 
-			// get the current working directory
-			wd, err := os.Getwd()
-			if err != nil {
-				return err
-			}
-
 			// load the config
 			cfg, err := config.Load(home, env)
 			if err != nil {
 				return err
 			}
 
+			// get the current working directory
+			wd, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+
 			// check each of the sites for a match
-			var currentSite string
+			var site string
 			var sites []string
-			for _, site := range cfg.Sites {
+			for _, s := range cfg.Sites {
 				// get the path
-				path, _ := site.GetAbsPath(home)
+				path, _ := s.GetAbsPath(home)
 
 				// see if the sites path matches the current directory
 				if strings.Contains(wd, path) {
-					currentSite = site.Hostname
+					site = s.Hostname
 					break
 				}
 
 				// add the site as an option
-				sites = append(sites, site.Hostname)
+				sites = append(sites, s.Hostname)
 			}
 
-			if currentSite == "" {
+			// if its not the current site
+			if site == "" {
 				// show all of the sites to the user
 				selected, err := output.Select(cmd.InOrStdin(), "Select a site: ", sites)
 				if err != nil {
 					return err
 				}
 
-				currentSite = sites[selected]
+				site = sites[selected]
+			}
+
+			// enable xdebug for the sites hostname
+			if err := cfg.EnableXdebug(site); err != nil {
+				return err
+			}
+
+			// save the config
+			if err := cfg.Save(); err != nil {
+				return err
 			}
 
 			return fmt.Errorf("use config.EnableXdebug(site) and call apply")
