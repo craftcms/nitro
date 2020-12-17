d6fc76c9bf800a44a9abb3208bb7431fc01099c4
Jason McCallister <jason@craftcms.com>
2020-12-17 13:28:36 -0500

    add wip nginx pkg for generating the config

---
+++ command/apply/internal/nginx/nginx.go
@@ -0,0 +1,50 @@
+package nginx
+
+import "fmt"
+
+var conf = `server {
+    listen      8080 default_server;
+    listen      [::]:8080 default_server;
+    server_name _;
+    set         $base /app;
+    root        $base/%s;
+
+    # security
+    include     craftcms/security.conf;
+
+    # index.php
+    index       index.php;
+
+    # index.php fallback
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    # additional config
+    include craftcms/general.conf;
+
+    # handle .php
+    location ~ \.php$ {
+        include craftcms/php_fastcgi.conf;
+    }
+
+    # Allow fpm ping and status from localhost
+    location ~ ^/(fpm-status|fpm-ping)$ {
+        access_log off;
+        allow 127.0.0.1;
+        deny all;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        include fastcgi_params;
+        fastcgi_pass 127.0.0.1:9000;
+    }
+}`
+
+// Generate takes a root directort generates a nginx configuration file
+func Generate(root string) string {
+	// if the root was not provided, default to web
+	if root == "" {
+		return fmt.Sprintf(conf, "web")
+	}
+
+	return fmt.Sprintf(conf, root)
+}
---
+++ command/apply/internal/nginx/nginx_test.go
@@ -0,0 +1,66 @@
+package nginx
+
+import "testing"
+
+func TestGenerate(t *testing.T) {
+	type args struct {
+		root string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "defaults to web",
+			args: args{
+				root: "",
+			},
+			want: defaultConf,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Generate(tt.args.root); got != tt.want {
+				t.Errorf("Generate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var defaultConf = `server {
+    listen      8080 default_server;
+    listen      [::]:8080 default_server;
+    server_name _;
+    set         $base /app;
+    root        $base/web;
+
+    # security
+    include     craftcms/security.conf;
+
+    # index.php
+    index       index.php;
+
+    # index.php fallback
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    # additional config
+    include craftcms/general.conf;
+
+    # handle .php
+    location ~ \.php$ {
+        include craftcms/php_fastcgi.conf;
+    }
+
+    # Allow fpm ping and status from localhost
+    location ~ ^/(fpm-status|fpm-ping)$ {
+        access_log off;
+        allow 127.0.0.1;
+        deny all;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        include fastcgi_params;
+        fastcgi_pass 127.0.0.1:9000;
+    }
+}`
