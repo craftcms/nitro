bb05664d23e544f300ce12f342d327779dde936d
Jason McCallister <jason@craftcms.com>
2020-12-17 11:17:28 -0500

    cleanup

--- client/client.go
+++ client/client.go
@@ -7,9 +7,8 @@ import (
 	"google.golang.org/grpc"
 )
 
-// NewClient is used for generating a new client
-// to interact with the gRPC API running in the
-// proxy container
+// NewClient is used for generating a new client to interact
+// with the gRPC API running in the proxy container
 func NewClient(ip, port string) (protob.NitroClient, error) {
 	cc, err := grpc.Dial(ip+":"+port, grpc.WithInsecure())
 	if err != nil {
