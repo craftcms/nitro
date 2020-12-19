168ec7b8c7916fce1cf6fa5c87dd051a29218fbf
Jason McCallister <jason@craftcms.com>
2020-12-19 13:16:21 -0500

    add prompts to first time setup

--- setup/setup.go
+++ setup/setup.go
@@ -1,7 +1,9 @@
 package setup
 
 import (
+	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/craftcms/nitro/config"
 	"github.com/craftcms/nitro/terminal"
@@ -17,19 +19,54 @@ func FirstTime(home, env string, output terminal.Outputer) error {
 		File: filepath.Join(home, ".nitro", env+".yaml"),
 	}
 
-	// add a default mysql database
-	c.Databases = append(c.Databases, config.Database{
-		Engine:  "mysql",
-		Version: "8.0",
-		Port:    "3306",
-	})
+	mysql, err := confirm("Would you like to use MySQL", true)
+	if err != nil {
+		return err
+	}
 
-	// add a default postgres database
-	c.Databases = append(c.Databases, config.Database{
-		Engine:  "postgres",
-		Version: "12",
-		Port:    "5432",
-	})
+	if mysql {
+		output.Pending("adding mysql 8.0 on port 3306")
+
+		// add a default mysql database
+		c.Databases = append(c.Databases, config.Database{
+			Engine:  "mysql",
+			Version: "8.0",
+			Port:    "3306",
+		})
+
+		output.Done()
+	}
+
+	postgres, err := confirm("Would you like to use PostgreSQL", true)
+	if err != nil {
+		return err
+	}
+
+	if postgres {
+		output.Pending("adding postgres 12 on port 5432")
+
+		// add a default postgres database
+		c.Databases = append(c.Databases, config.Database{
+			Engine:  "postgres",
+			Version: "12",
+			Port:    "5432",
+		})
+
+		output.Done()
+	}
+
+	redis, err := confirm("Would you like to setup Redis", true)
+	if err != nil {
+		return err
+	}
+
+	if redis {
+		output.Pending("adding redis 6379")
+
+		c.Services.Redis = true
+
+		output.Done()
+	}
 
 	// save the file
 	if err := c.Save(); err != nil {
@@ -37,4 +74,77 @@ func FirstTime(home, env string, output terminal.Outputer) error {
 	}
 
 	return nil
+}
+
+func confirm(msg string, fallback bool) (bool, error) {
+	if fallback {
+		fmt.Print(fmt.Sprintf("%s [Y/n]? ", msg))
+	} else {
+		fmt.Print(fmt.Sprintf("%s [y/N]? ", msg))
+	}
+
+	// prompt for confirmation
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil && err.Error() != "unexpected newline" {
+		return false, fmt.Errorf("unable to provide a prompt, %w", err)
+	}
+
+	// check for empty newline and use the default
+	if err.Error() == "unexpected newline" {
+		return fallback, nil
+	}
+
+	// read the confirmation from the input
+	var confirm bool
+	resp := strings.TrimSpace(input)
+	for _, answer := range []string{"y", "Y", "yes", "Yes", "YES"} {
+		if resp == answer {
+			confirm = true
+		}
+	}
+
+	return confirm, nil
+}
+
+func setupPostgres() (bool, error) {
+	fmt.Print("Would you like to use PostgreSQL [Y/n]? ")
+
+	// prompt the user for confirmation
+	var response string
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		return false, fmt.Errorf("unable to provide a prompt, %w", err)
+	}
+
+	var confirm bool
+	resp := strings.TrimSpace(response)
+	for _, answer := range []string{"y", "Y", "yes", "Yes", "YES"} {
+		if resp == answer {
+			confirm = true
+		}
+	}
+
+	return confirm, nil
+}
+
+func setupRedis() (bool, error) {
+	fmt.Print("Would you like to use Redis [Y/n]? ")
+
+	// prompt the user for confirmation
+	var response string
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		return false, fmt.Errorf("unable to provide a prompt, %w", err)
+	}
+
+	var confirm bool
+	resp := strings.TrimSpace(response)
+	for _, answer := range []string{"y", "Y", "yes", "Yes", "YES"} {
+		if resp == answer {
+			confirm = true
+		}
+	}
+
+	return confirm, nil
 }
