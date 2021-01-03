002d04ff40b49dd641951d630f3d4ab0e4b1bfcb
Jason McCallister <jason@craftcms.com>
2021-01-03 14:37:28 -0500

    docs

--- pkg/config/site.go
+++ pkg/config/site.go
@@ -52,6 +52,8 @@ func (s *Site) AsEnvs(addr string) []string {
 		addr = "host.docker.internal"
 	}
 
+	// set the composer home so we can install plugins and
+	// updates from the control panel
 	envs = append(envs, "COMPOSER_HOME=/tmp")
 
 	// if they do not specify the error... false means on
