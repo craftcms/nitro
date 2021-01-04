ed90d6e7b56617b97171b0fa86a458d703876b75
Jason McCallister <jason@craftcms.com>
2021-01-04 17:45:51 -0500

    refactor envs for sites and mounts

--- pkg/config/site.go
+++ pkg/config/site.go
@@ -52,70 +52,91 @@ func (s *Site) AsEnvs(addr string) []string {
 		addr = "host.docker.internal"
 	}
 
+	// set the php vars
+	envs = append(envs, phpVars(envs, s.PHP, s.Version)...)
+
+	// get the xdebug vars
+	envs = append(envs, xdebugVars(envs, s.PHP, s.Xdebug, s.Version, addr)...)
+
+	// set the blackfire envs if available
+	// if s.Blackfire.ServerID != "" {
+	// 	envs = append(envs, "BLACKFIRE_SERVER_ID="+s.Blackfire.ServerID)
+	// }
+	// if s.Blackfire.ServerToken != "" {
+	// 	envs = append(envs, "BLACKFIRE_SERVER_TOKEN="+s.Blackfire.ServerToken)
+	// }
+
+	return envs
+}
+
+func phpVars(envs []string, php PHP, version string) []string {
 	// set the composer home so we can install plugins and
 	// updates from the control panel
 	envs = append(envs, "COMPOSER_HOME=/tmp")
 
 	// if they do not specify the error... false means on
-	if !s.PHP.DisplayErrors {
+	if !php.DisplayErrors {
 		envs = append(envs, "PHP_DISPLAY_ERRORS="+DefaultEnvs["PHP_DISPLAY_ERRORS"])
 	} else {
 		envs = append(envs, "PHP_DISPLAY_ERRORS=off")
 	}
 
-	if s.PHP.MemoryLimit == "" {
+	if php.MemoryLimit == "" {
 		envs = append(envs, "PHP_MEMORY_LIMIT="+DefaultEnvs["PHP_MEMORY_LIMIT"])
 	} else {
-		envs = append(envs, "PHP_MEMORY_LIMIT="+s.PHP.MemoryLimit)
+		envs = append(envs, "PHP_MEMORY_LIMIT="+php.MemoryLimit)
 	}
 
-	if s.PHP.MaxExecutionTime == 0 {
+	if php.MaxExecutionTime == 0 {
 		envs = append(envs, "PHP_MAX_EXECUTION_TIME="+DefaultEnvs["PHP_MAX_EXECUTION_TIME"])
 	} else {
-		envs = append(envs, fmt.Sprintf("%s=%d", "PHP_MAX_EXECUTION_TIME", s.PHP.MaxExecutionTime))
+		envs = append(envs, fmt.Sprintf("%s=%d", "PHP_MAX_EXECUTION_TIME", php.MaxExecutionTime))
 	}
 
-	if s.PHP.UploadMaxFileSize == "" {
+	if php.UploadMaxFileSize == "" {
 		envs = append(envs, "PHP_UPLOAD_MAX_FILESIZE="+DefaultEnvs["PHP_UPLOAD_MAX_FILESIZE"])
 	} else {
-		envs = append(envs, "PHP_UPLOAD_MAX_FILESIZE="+s.PHP.UploadMaxFileSize)
+		envs = append(envs, "PHP_UPLOAD_MAX_FILESIZE="+php.UploadMaxFileSize)
 	}
 
-	if s.PHP.MaxInputVars == 0 {
+	if php.MaxInputVars == 0 {
 		envs = append(envs, "PHP_MAX_INPUT_VARS="+DefaultEnvs["PHP_MAX_INPUT_VARS"])
 	} else {
-		envs = append(envs, fmt.Sprintf("%s=%d", "PHP_MAX_INPUT_VARS", s.PHP.MaxInputVars))
+		envs = append(envs, fmt.Sprintf("%s=%d", "PHP_MAX_INPUT_VARS", php.MaxInputVars))
 	}
 
-	if s.PHP.PostMaxSize == "" {
+	if php.PostMaxSize == "" {
 		envs = append(envs, "PHP_POST_MAX_SIZE="+DefaultEnvs["PHP_POST_MAX_SIZE"])
 	} else {
-		envs = append(envs, fmt.Sprintf("%s=%s", "PHP_POST_MAX_SIZE", s.PHP.PostMaxSize))
+		envs = append(envs, fmt.Sprintf("%s=%s", "PHP_POST_MAX_SIZE", php.PostMaxSize))
 	}
 
 	// handle opcache settings
-	if s.PHP.OpcacheEnable {
+	if php.OpcacheEnable {
 		envs = append(envs, "PHP_OPCACHE_ENABLE=1")
 	} else {
 		envs = append(envs, "PHP_OPCACHE_ENABLE="+DefaultEnvs["PHP_OPCACHE_ENABLE"])
 	}
 
-	if s.PHP.OpcacheRevalidateFreq == 0 {
+	if php.OpcacheRevalidateFreq == 0 {
 		envs = append(envs, "PHP_OPCACHE_REVALIDATE_FREQ="+DefaultEnvs["PHP_OPCACHE_REVALIDATE_FREQ"])
 	} else {
-		envs = append(envs, fmt.Sprintf("PHP_OPCACHE_REVALIDATE_FREQ=%d", s.PHP.OpcacheRevalidateFreq))
+		envs = append(envs, fmt.Sprintf("PHP_OPCACHE_REVALIDATE_FREQ=%d", php.OpcacheRevalidateFreq))
 
 	}
 
 	// always set the session
 	envs = append(envs, "XDEBUG_SESSION=PHPSTORM")
 
-	// check if xdebug is enabled
-	switch s.Xdebug {
+	return envs
+}
+
+func xdebugVars(envs []string, php PHP, xdebug bool, version, addr string) []string {
+	switch xdebug {
 	case false:
 		envs = append(envs, "XDEBUG_MODE=off")
 	default:
-		switch s.Version {
+		switch version {
 		case "8.0", "7.4", "7.3", "7.2":
 			envs = append(envs, fmt.Sprintf(`XDEBUG_CONFIG=client_host=%s start_with_request=yes discover_client_host=1`, addr))
 			envs = append(envs, "XDEBUG_MODE=develop,debug")
@@ -125,14 +146,6 @@ func (s *Site) AsEnvs(addr string) []string {
 			envs = append(envs, "XDEBUG_MODE=xdebug2")
 		}
 	}
-
-	// set the blackfire envs if available
-	// if s.Blackfire.ServerID != "" {
-	// 	envs = append(envs, "BLACKFIRE_SERVER_ID="+s.Blackfire.ServerID)
-	// }
-	// if s.Blackfire.ServerToken != "" {
-	// 	envs = append(envs, "BLACKFIRE_SERVER_TOKEN="+s.Blackfire.ServerToken)
-	// }
 
 	return envs
 }
