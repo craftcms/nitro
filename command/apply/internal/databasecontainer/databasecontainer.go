7745587092cf2b9b593eca3328b72bdc1682cd89
Jason McCallister <jason@craftcms.com>
2021-01-26 10:16:33 -0500

    don't use mysqld

--- command/apply/internal/databasecontainer/databasecontainer.go
+++ command/apply/internal/databasecontainer/databasecontainer.go
@@ -150,7 +150,7 @@ func StartOrCreate(ctx context.Context, docker client.CommonAPIClient, networkID
 
 	// if the mysql engine is being used, override the cmd
 	if db.Engine == "mysql" {
-		containerConfig.Cmd = []string{"/usr/sbin/mysqld", "--character-set-server=utf8mb4", "--collation-server=utf8mb4_unicode_ci"}
+		containerConfig.Cmd = []string{"--character-set-server=utf8mb4", "--collation-server=utf8mb4_unicode_ci"}
 	}
 
 	hostConfig := &container.HostConfig{
