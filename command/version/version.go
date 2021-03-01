8d79d8ee66c79569d00889d331ea961b5ccc93b0
Jason McCallister <jason@craftcms.com>
2021-03-01 08:10:33 -0500

    fallback to container label when checking version

--- command/version/version.go
+++ command/version/version.go
@@ -6,6 +6,7 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
 
+	"github.com/craftcms/nitro/pkg/labels"
 	"github.com/craftcms/nitro/pkg/terminal"
 	"github.com/craftcms/nitro/protob"
 )
@@ -24,9 +25,23 @@ func New(client client.CommonAPIClient, nitrod protob.NitroClient, output termin
 		Short:   "Show version info",
 		Example: exampleText,
 		RunE: func(cmd *cobra.Command, args []string) error {
+			var vers string
 			nitro, err := nitrod.Version(cmd.Context(), &protob.VersionRequest{})
 			if err != nil {
 				return fmt.Errorf("unable to get version from the gRPC API")
+			}
+
+			vers = nitro.GetVersion()
+
+			// make sure the version is not empty
+			if vers == "" {
+				// look up the version from the container label
+				details, err := client.ContainerInspect(cmd.Context(), "nitro-proxy")
+				if err != nil {
+					return err
+				}
+
+				vers = details.Config.Labels[labels.ProxyVersion]
 			}
 
 			ver, err := client.ServerVersion(cmd.Context())
@@ -37,12 +52,12 @@ func New(client client.CommonAPIClient, nitrod protob.NitroClient, output termin
 			output.Info(fmt.Sprintf("View the changelog at https://github.com/craftcms/nitro/blob/%s/CHANGELOG.md\n", Version))
 
 			output.Info("Nitro CLI: \t", Version)
-			output.Info("Nitro gRPC: \t", nitro.GetVersion())
+			output.Info("Nitro gRPC: \t", vers)
 			output.Info("Docker API: \t", ver.APIVersion, "("+ver.MinAPIVersion+" min)")
 			output.Info("Docker CLI: \t", client.ClientVersion())
 
 			// check if the cli and API do not match
-			if Version != nitro.GetVersion() {
+			if Version != vers {
 				output.Info("")
 				output.Info("The Nitro CLI and gRPC versions do not match")
 				output.Info("You might need to run `nitro update`")
