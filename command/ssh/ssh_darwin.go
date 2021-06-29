c910c9f8d67f9ce726cc286332bd7a857fe92f22
Jason McCallister <jason@craftcms.com>
2021-06-29 18:37:49 -0400

    change the user to nitro

--- command/ssh/ssh_darwin.go
+++ command/ssh/ssh_darwin.go
@@ -178,7 +178,7 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 			}
 
 			// check if the root user should be used
-			containerUser := "www-data"
+			containerUser := "nitro"
 			if RootUser || ProxyContainer {
 				containerUser = "root"
 			}
@@ -188,7 +188,7 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 				output.Info("using root… system changes are ephemeral…")
 			}
 
-			c := exec.Command(cli, "exec", "-u", containerUser, "-it", containerID, "sh")
+			c := exec.Command(cli, "exec", "-u", containerUser, "-it", containerID, "bash")
 
 			c.Stdin = cmd.InOrStdin()
 			c.Stderr = cmd.ErrOrStderr()
