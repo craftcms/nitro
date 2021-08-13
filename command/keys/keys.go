57fb48d3c0cea9d3d766aa246f1fa5b07557f7b9
Jason McCallister <jason@craftcms.com>
2021-08-12 23:26:32 -0400

    improving the site args for selecting a key and a site

--- command/keys/keys.go
+++ command/keys/keys.go
@@ -9,13 +9,13 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
 
+	"github.com/craftcms/nitro/pkg/config"
 	"github.com/craftcms/nitro/pkg/keys"
 	"github.com/craftcms/nitro/pkg/terminal"
 )
 
 var (
-	// ErrExample is used when we want to share an error
-	ErrExample = fmt.Errorf("not yet implemented")
+	site *config.Site
 )
 
 const exampleText = `  # keys command
@@ -26,9 +26,67 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 		Use:     "keys",
 		Short:   "Adds SSH keys to a site container.",
 		Example: exampleText,
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			cfg, err := config.Load(home)
+			if err != nil {
+				return nil, cobra.ShellCompDirectiveDefault
+			}
+
+			var options []string
+			for _, s := range cfg.Sites {
+				options = append(options, s.Hostname)
+			}
+
+			return options, cobra.ShellCompDirectiveNoFileComp
+		},
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			// load the config
+			cfg, err := config.Load(home)
+			if err != nil {
+				return err
+			}
+
+			// get the current working directory
+			wd, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+
+			// is there a site as the first arg?
+			if len(args) > 0 {
+				site, err = cfg.FindSiteByHostName(args[0])
+				if err != nil {
+					return err
+				}
+
+				output.Info("Preparing key import to", site.Hostname)
+
+				return nil
+			}
+
+			// get a context aware list of sites
+			sites := cfg.ListOfSitesByDirectory(home, wd)
+
+			// create the options for the sites
+			var options []string
+			for _, s := range sites {
+				options = append(options, s.Hostname)
+			}
+
+			// prompt for the site to ssh into
+			selected, err := output.Select(cmd.InOrStdin(), "Select a site: ", options)
+			if err != nil {
+				return err
+			}
+
+			site = &sites[selected]
+
+			output.Info("Preparing key import to", site.Hostname)
+
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			path := filepath.Join(home, ".ssh")
-
 			if _, err := os.Stat(path); os.IsNotExist(err) {
 				return errors.New("unable to find directory " + path)
 			}
@@ -46,7 +104,7 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 			}
 
 			var opts []string
-			for k, _ := range keys {
+			for k := range keys {
 				opts = append(opts, k)
 			}
 
@@ -55,7 +113,7 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 				return err
 			}
 
-			return ErrExample
+			return nil
 		},
 	}
 
