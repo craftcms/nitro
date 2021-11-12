a559d75ce381e384ba8b094dd262e888dfcbf6bc
Jason McCallister <jason@craftcms.com>
2021-11-12 11:11:27 -0500

    update darwin to use new flag and pkg

--- command/ssh/ssh_darwin.go
+++ command/ssh/ssh_darwin.go
@@ -1,13 +1,15 @@
-// +build darwin, !linux
+//go:build darwin || !linux
+// +build darwin !linux
 
 package ssh
 
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"strings"
 
+	"github.com/craftcms/nitro/pkg/appaware"
+	"github.com/craftcms/nitro/pkg/dockerexec"
+	"github.com/craftcms/nitro/pkg/flags"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
@@ -47,122 +49,9 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 				return err
 			}
 
-			var site string
-			if len(args) > 0 {
-				site = strings.TrimSpace(args[0])
-			}
-
 			// create a filter for the environment
 			filter := filters.NewArgs()
 			filter.Add("label", containerlabels.Nitro)
-
-			var containerID string
-			switch ProxyContainer {
-			case true:
-				// file by the container name
-				filter.Add("name", proxycontainer.ProxyName)
-
-				// find the containers but limited to the site label
-				containers, err := docker.ContainerList(cmd.Context(), types.ContainerListOptions{Filters: filter, All: true})
-				if err != nil {
-					return err
-				}
-
-				if len(containers) == 0 {
-					return fmt.Errorf("no containers found")
-				}
-
-				// start the container if its not running
-				if containers[0].State != "running" {
-					for _, command := range cmd.Root().Commands() {
-						if command.Use == "start" {
-							if err := command.RunE(cmd, []string{}); err != nil {
-								return err
-							}
-						}
-					}
-				}
-
-				containerID = containers[0].ID
-			default:
-				// get a context aware list of sites
-				sites := cfg.ListOfSitesByDirectory(home, wd)
-
-				// create the options for the sites
-				var options []string
-				for _, s := range sites {
-					options = append(options, s.Hostname)
-				}
-
-				// did they ask for a specific site?
-				switch site != "" {
-				case true:
-					for k, v := range options {
-						if site == v {
-							// add the label to get the site
-							filter.Add("label", containerlabels.Host+"="+sites[k].Hostname)
-							break
-						}
-					}
-				default:
-					// if there are found sites we want to show or connect to the first one, otherwise prompt for which site to connect to.
-					switch len(sites) {
-					case 0:
-						// prompt for the site to ssh into
-						selected, err := output.Select(cmd.InOrStdin(), "Select a site: ", options)
-						if err != nil {
-							return err
-						}
-
-						// add the label to get the site
-						filter.Add("label", containerlabels.Host+"="+sites[selected].Hostname)
-					case 1:
-						output.Info("connecting to", sites[0].Hostname)
-
-						// add the label to get the site
-						filter.Add("label", containerlabels.Host+"="+sites[0].Hostname)
-					default:
-						// prompt for the site to ssh into
-						selected, err := output.Select(cmd.InOrStdin(), "Select a site: ", options)
-						if err != nil {
-							return err
-						}
-
-						// add the label to get the site
-						filter.Add("label", containerlabels.Host+"="+sites[selected].Hostname)
-					}
-				}
-
-				// find the containers but limited to the site label
-				containers, err := docker.ContainerList(cmd.Context(), types.ContainerListOptions{Filters: filter, All: true})
-				if err != nil {
-					return err
-				}
-
-				// are there any containers??
-				if len(containers) == 0 {
-					return fmt.Errorf("unable to find an matching site")
-				}
-
-				// start the container if its not running
-				if containers[0].State != "running" {
-					for _, command := range cmd.Root().Commands() {
-						if command.Use == "start" {
-							if err := command.RunE(cmd, []string{}); err != nil {
-								return err
-							}
-						}
-					}
-				}
-
-				containerID = containers[0].ID
-			}
-
-			// find the docker executable
-			cli, err := exec.LookPath("docker")
-			if err != nil {
-				return err
-			}
 
 			// check if the root user should be used
 			containerUser := "nitro"
@@ -180,13 +69,95 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 				shell = "sh"
 			}
 
-			c := exec.Command(cli, "exec", "-u", containerUser, "-it", containerID, shell)
+			var containerID string
+			switch ProxyContainer {
+			case true:
+				// file by the container name
+				filter.Add("name", proxycontainer.ProxyName)
 
-			c.Stdin = cmd.InOrStdin()
-			c.Stderr = cmd.ErrOrStderr()
-			c.Stdout = cmd.OutOrStdout()
+				// find the containers but limited to the site label
+				containers, err := docker.ContainerList(cmd.Context(), types.ContainerListOptions{Filters: filter, All: true})
+				if err != nil {
+					return err
+				}
 
-			return c.Run()
+				if len(containers) == 0 {
+					return fmt.Errorf("no containers found")
+				}
+
+				// start the container if not running
+				if containers[0].State != "running" {
+					if err := docker.ContainerStart(cmd.Context(), containers[0].ID, types.ContainerStartOptions{}); err != nil {
+						return err
+					}
+				}
+
+				containerID = containers[0].ID
+			default:
+				// is there a global flag for the app?
+				if flags.AppName != "" {
+					// find the app by the hostname
+					app, err :=cfg.FindAppByHostname(flags.AppName)
+					if err != nil {
+						return err
+					}
+
+					// add the label to get the site
+					filter.Add("label", containerlabels.Host+"="+app.Hostname)
+
+					// find the containers but limited to the app label
+					containers, err := docker.ContainerList(cmd.Context(), types.ContainerListOptions{Filters: filter, All: true})
+					if err != nil {
+						return err
+					}
+
+					// are there any containers??
+					if len(containers) == 0 {
+						return fmt.Errorf("unable to find an matching site")
+					}
+
+					// start the container if not running
+					if containers[0].State != "running" {
+						if err := docker.ContainerStart(cmd.Context(), containers[0].ID, types.ContainerStartOptions{}); err != nil {
+							return err
+						}
+					}
+
+					containerID = containers[0].ID
+
+					break
+				}
+
+				appName, err := appaware.Detect(*cfg, wd)
+				if err != nil {
+					return err
+				}
+
+				// add the label to get the site
+				filter.Add("label", containerlabels.Host+"="+appName)
+
+				// find the containers but limited to the app label
+				containers, err := docker.ContainerList(cmd.Context(), types.ContainerListOptions{Filters: filter, All: true})
+				if err != nil {
+					return err
+				}
+
+				// are there any containers??
+				if len(containers) == 0 {
+					return fmt.Errorf("unable to find an matching site")
+				}
+
+				// start the container if not running
+				if containers[0].State != "running" {
+					if err := docker.ContainerStart(cmd.Context(), containers[0].ID, types.ContainerStartOptions{}); err != nil {
+						return err
+					}
+				}
+
+				containerID = containers[0].ID
+			}
+
+			return dockerexec.Connect(cmd.InOrStdin(), cmd.OutOrStdout(), containerUser, containerID, shell)
 		},
 	}
 
