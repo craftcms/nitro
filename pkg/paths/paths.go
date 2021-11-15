57d704f0ddd66c0d75a242bb51d16139209e8c2e
Jason McCallister <jason@craftcms.com>
2021-11-15 09:23:03 -0500

    add MakeRelative func

--- pkg/paths/paths.go
+++ pkg/paths/paths.go
@@ -20,3 +20,16 @@ func Clean(home, path string) (string, error) {
 
 	return filepath.Clean(abs), nil
 }
+
+// MakeRelative takes a users home directory and a path and returns the relative (using
+// ~/path) path and takes an optional boolean to strip the trailing slash.
+func MakeRelative(home, dir string, strip bool) string {
+	p := strings.Replace(dir, home, "~", 1)
+
+	// should we remove the trailing slash?
+	if strip {
+		p = strings.TrimRight(p, "/")
+	}
+
+	return p
+}
