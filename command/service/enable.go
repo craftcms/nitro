a5cbae5fe7e95af30c07a2dd2ef28933a1fab06c
Jason McCallister <jason@craftcms.com>
2021-11-23 10:21:58 -0500

    fix merge conflicts

--- command/service/enable.go
+++ command/service/enable.go
@@ -1,10 +1,8 @@
-package enable
+package service
 
 import (
-	"os"
+	"fmt"
 
-	"github.com/craftcms/nitro/pkg/appaware"
-	"github.com/craftcms/nitro/pkg/flags"
 	"github.com/craftcms/nitro/pkg/prompt"
 	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
@@ -13,54 +11,67 @@ import (
 	"github.com/craftcms/nitro/pkg/terminal"
 )
 
-const exampleText = `  # enable the app in the current directory
-  nitro enable
-
-  # enable a specific app using the global flag
-  nitro --app myapp.nitro enable`
-
 // NewCommand returns the command to enable an app from automatically starting.
-func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outputer) *cobra.Command {
+func enableCommand(home string, docker client.CommonAPIClient, output terminal.Outputer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "enable",
 		Short: "Enables an app.",
-		Example:   exampleText,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				fmt.Println(cmd.UsageString())
+
+				return fmt.Errorf("service name param missing")
+			}
+
+			return nil
+		},
+		ValidArgs: []string{"blackfire", "dynamodb", "mailhog", "minio", "redis"},
+		Example: `  # enable services
+  nitro service enable <service-name>
+
+  # enable blackfire
+  nitro service enable blackfire
+
+  # enable mailhog
+  nitro service enable mailhog
+
+  # enable minio
+  nitro service enable minio
+
+  # enable dynamodb
+  nitro service enable dynamodb`,
 		PostRunE: func(cmd *cobra.Command, args []string) error {
 			return prompt.RunApply(cmd, args, false, output)
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// load the config
+			// load the configuration
 			cfg, err := config.Load(home)
 			if err != nil {
 				return err
 			}
 
-			// get the app
-			name := flags.AppName
-			if name == "" {
-				// get the current working directory
-				wd, err := os.Getwd()
-				if err != nil {
-					return err
-				}
-
-				name, err = appaware.Detect(*cfg, wd)
-				if err != nil {
-					return err
-				}
+			// disable the service
+			switch args[0] {
+			case "blackfire":
+				cfg.Services.Blackfire = false
+			case "dynamodb":
+				cfg.Services.DynamoDB = false
+			case "mailhog":
+				cfg.Services.Mailhog = false
+			case "minio":
+				cfg.Services.Minio = false
+			case "redis":
+				cfg.Services.Redis = false
+			default:
+				return ErrUnknownService
 			}
 
-			// disable the app
-			if err := cfg.EnableApp(name); err != nil {
-				return err
-			}
-
-			// save the config
+			// save the config file
 			if err := cfg.Save(); err != nil {
-				return err
+				return fmt.Errorf("unable to save config, %w", err)
 			}
 
-			output.Info("Enabled", name)
+			output.Info("Enabled", args[0])
 
 			return nil
 		},
