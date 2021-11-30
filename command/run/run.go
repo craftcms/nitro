13def6b01a62acffe2af05902074d14622465cb9
Jason McCallister <jason@craftcms.com>
2021-11-30 11:27:57 -0500

    cleanup

--- command/run/run.go
+++ command/run/run.go
@@ -11,6 +11,11 @@ import (
 	"github.com/craftcms/nitro/pkg/terminal"
 )
 
+var (
+	flagImage, flagWorkingDir   string
+	flagInteractive, flagRemove bool
+)
+
 const exampleText = `  # run one off containers
   nitro run`
 
@@ -34,32 +39,32 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 			c.Stdout = cmd.OutOrStdout()
 
 			// should the container bew removed after completion?
-			if cmd.Flag("remove").Value.String() == "true" {
+			if flagRemove {
 				c.Args = append(c.Args, "--rm")
 			}
 
 			// should the container be interactive
-			if cmd.Flag("interactive").Value.String() == "true" {
+			if flagInteractive {
 				c.Args = append(c.Args, "-it")
 			}
 
 			// if the working dir is set, grab the current directory and mount it
-			if cmd.Flag("working-dir").Value.String() != "" {
+			if flagWorkingDir != "" {
 				// get the working dir
-				wd, err := os.Getwd()
+				current, err := os.Getwd()
 				if err != nil {
 					return err
 				}
 
 				c.Args = append(c.Args, "-v")
 
-				vol := fmt.Sprintf("%s:%s", wd, cmd.Flag("working-dir").Value.String())
+				vol := fmt.Sprintf("%s:%s", current, flagWorkingDir)
 
 				c.Args = append(c.Args, vol)
 			}
 
 			// set the image to use, if the image is not found docker will pull it
-			c.Args = append(c.Args, cmd.Flag("image").Value.String())
+			c.Args = append(c.Args, flagImage)
 
 			// append the args to the container
 			c.Args = append(c.Args, args...)
@@ -71,10 +76,11 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 	}
 
 	// set flags for the command
-	cmd.Flags().String("working-dir", "", "sets the working directory for the container")
-	cmd.Flags().Bool("interactive", true, "")
-	cmd.Flags().String("image", "", "")
-	cmd.Flags().Bool("remove", true, "")
+	cmd.Flags().StringVar(&flagWorkingDir, "working-dir", "", "working directory for the container")
+	cmd.Flags().BoolVar(&flagInteractive, "interactive", true, "should the container be interactive?")
+	cmd.Flags().StringVar(&flagImage, "image", "", "image to use for the container")
+	cmd.Flags().BoolVar(&flagRemove, "persist", true, "persist container after completion")
+	cmd.MarkFlagRequired("image")
 
 	return cmd
 }
