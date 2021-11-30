f9acbff3922a6b71eb08fee1d089d01ffbfc5bfd
Jason McCallister <jason@craftcms.com>
2021-11-30 06:46:02 -0500

    add the run command

---
+++ command/run/run.go
@@ -0,0 +1,55 @@
+package run
+
+import (
+	"os/exec"
+
+	"github.com/docker/docker/client"
+	"github.com/spf13/cobra"
+
+	"github.com/craftcms/nitro/pkg/terminal"
+)
+
+const exampleText = `  # run one off containers
+  nitro run`
+
+func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outputer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "run",
+		Short:   "Runs a container.",
+		Example: exampleText,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path, err := exec.LookPath("docker")
+			if err != nil {
+				return err
+			}
+
+			c := exec.Command(path, "run")
+
+			c.Stdin = cmd.InOrStdin()
+			c.Stderr = cmd.ErrOrStderr()
+			c.Stdout = cmd.OutOrStdout()
+
+			if cmd.Flag("remove").Value.String() == "true" {
+				c.Args = append(c.Args, "--rm")
+			}
+
+			if cmd.Flag("interactive").Value.String() == "true" {
+				c.Args = append(c.Args, "-it")
+			}
+
+			c.Args = append(c.Args, cmd.Flag("image").Value.String())
+
+			// TODO(jasonmccallister) grab the args for the container commands
+
+			return c.Run()
+		},
+	}
+
+	// set flags for the command
+	cmd.Flags().String("working-dir", "", "sets the working directory for the container")
+	cmd.Flags().Bool("interactive", true, "")
+	cmd.Flags().String("image", "", "")
+	cmd.Flags().Bool("remove", true, "")
+
+	return cmd
+}
