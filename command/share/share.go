e19f42747f2374c0ca0240bc51607849221ad908
Jason McCallister <jason@craftcms.com>
2022-05-19 12:40:19 -0400

    use --host-header

--- command/share/share.go
+++ command/share/share.go
@@ -163,7 +163,7 @@ func NewCommand(home string, docker client.CommonAPIClient, output terminal.Outp
 			ngrokArgs := []string{"http"}
 
 			// set the main hostname
-			ngrokArgs = append(ngrokArgs, "-host-header="+site.Hostname)
+			ngrokArgs = append(ngrokArgs, "--host-header="+site.Hostname)
 
 			// append the aliases
 			for _, a := range site.Aliases {
